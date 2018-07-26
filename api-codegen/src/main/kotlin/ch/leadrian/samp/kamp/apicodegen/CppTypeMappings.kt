bb70d6dc12e339f360d3e0f0d8b25646ac2a8c66
leadrian <thewishwithin@gmail.com>
2018-07-26 22:33:20 +0200

    Added Header file native functions generator

---
+++ api-codegen/src/main/kotlin/ch/leadrian/samp/kamp/apicodegen/CppTypeMappings.kt
@@ -0,0 +1,24 @@
+package ch.leadrian.samp.kamp.apicodegen
+
+import ch.leadrian.samp.cidl.model.Types
+
+val CPP_TYPE_MAPPING = mapOf(
+        Types.BOOL to "jboolean",
+        Types.CHAR to "jchar",
+        Types.FLOAT to "jfloat",
+        Types.INT to "jint",
+        Types.STRING to "jstring",
+        Types.VOID to "void"
+)
+
+val CPP_OUT_TYPE_MAPPING = mapOf(
+        Types.FLOAT to "jobject",
+        Types.INT to "jobject",
+        Types.STRING to "jobject"
+)
+
+fun getCppType(typeName: String) =
+        CPP_TYPE_MAPPING[typeName] ?: throw IllegalStateException("Unknown C++ type: $typeName")
+
+fun getCppOutType(typeName: String) =
+        CPP_OUT_TYPE_MAPPING[typeName] ?: throw IllegalStateException("Unknown C++ out type: $typeName")
