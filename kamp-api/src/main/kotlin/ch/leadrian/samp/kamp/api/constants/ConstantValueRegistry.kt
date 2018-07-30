cc6b0253327959e0285a39f3c5786f91c21d2b3d
leadrian <thewishwithin@gmail.com>
2018-07-30 21:28:48 +0200

    Added ObjectMaterialTextAlignment

--- kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/constants/ConstantValueRegistry.kt
+++ kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/constants/ConstantValueRegistry.kt
@@ -2,14 +2,14 @@ package ch.leadrian.samp.kamp.api.constants
 
 open class ConstantValueRegistry<K, V : ConstantValue<K>>(vararg constants: V) {
 
-    private val sampConstantsByValue: MutableMap<K, V> = mutableMapOf()
+    private val constantsByValue: MutableMap<K, V> = mutableMapOf()
 
     init {
-        constants.forEach { sampConstantsByValue[it.value] = it }
+        constants.forEach { constantsByValue[it.value] = it }
     }
 
     operator fun get(value: K): V? =
-            sampConstantsByValue[value] ?: throw IllegalArgumentException("No constant with value $value")
+            constantsByValue[value] ?: throw IllegalArgumentException("No constant with value $value")
 
-    fun exists(value: K): Boolean = sampConstantsByValue.containsKey(value)
+    fun exists(value: K): Boolean = constantsByValue.containsKey(value)
 }
