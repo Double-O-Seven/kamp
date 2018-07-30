1d14351b05cd334e0b5a3b5696103374766fada7
leadrian <thewishwithin@gmail.com>
2018-07-30 21:42:30 +0200

    Added VehicleModel info

---
+++ kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/constants/ClickPlayerSource.kt
@@ -0,0 +1,8 @@
+package ch.leadrian.samp.kamp.api.constants
+
+enum class ClickPlayerSource(override val value: Int) : ConstantValue<Int> {
+    SCOREBOARD(SAMPConstants.CLICK_SOURCE_SCOREBOARD);
+
+    companion object : ConstantValueRegistry<Int, ClickPlayerSource>(*ClickPlayerSource.values())
+
+}
