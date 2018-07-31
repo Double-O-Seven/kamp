437dbb62dc2359b32335a228da03bcfff3794f94
leadrian <thewishwithin@gmail.com>
2018-07-31 23:30:13 +0200

    Added more Vectors factory methods

--- kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/data/Vectors.kt
+++ kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/data/Vectors.kt
@@ -18,9 +18,21 @@ fun vector3DOf(x: Float, y: Float, z: Float): Vector3D = Vector3DImpl(
         z = z
 )
 
+fun vector3DOf(coordinates: Vector2D, z: Float): Vector3D = Vector3DImpl(
+        x = coordinates.x,
+        y = coordinates.y,
+        z = z
+)
+
 fun mutableVector3DOf(x: Float, y: Float, z: Float): MutableVector3D = MutableVector3DImpl(
         x = x,
         y = y,
+        z = z
+)
+
+fun mutableVector3DOf(coordinates: Vector2D, z: Float): MutableVector3D = MutableVector3DImpl(
+        x = coordinates.x,
+        y = coordinates.y,
         z = z
 )
 
@@ -31,10 +43,24 @@ fun positionOf(x: Float, y: Float, z: Float, angle: Float): Position = PositionI
         angle = angle
 )
 
+fun positionOf(coordinates: Vector3D, angle: Float): Position = PositionImpl(
+        x = coordinates.x,
+        y = coordinates.y,
+        z = coordinates.z,
+        angle = angle
+)
+
 fun mutablePositionOf(x: Float, y: Float, z: Float, angle: Float): MutablePosition = MutablePositionImpl(
         x = x,
         y = y,
         z = z,
+        angle = angle
+)
+
+fun mutablePositionOf(coordinates: Vector3D, angle: Float): MutablePosition = MutablePositionImpl(
+        x = coordinates.x,
+        y = coordinates.y,
+        z = coordinates.z,
         angle = angle
 )
 
@@ -46,10 +72,26 @@ fun locationOf(x: Float, y: Float, z: Float, interiorId: Int, worldId: Int): Loc
         worldId = worldId
 )
 
+fun locationOf(coordinates: Vector3D, interiorId: Int, worldId: Int): Location = LocationImpl(
+        x = coordinates.x,
+        y = coordinates.y,
+        z = coordinates.z,
+        interiorId = interiorId,
+        worldId = worldId
+)
+
 fun mutableLocationOf(x: Float, y: Float, z: Float, interiorId: Int, worldId: Int): MutableLocation = MutableLocationImpl(
         x = x,
         y = y,
         z = z,
+        interiorId = interiorId,
+        worldId = worldId
+)
+
+fun mutableLocationOf(coordinates: Vector3D, interiorId: Int, worldId: Int): MutableLocation = MutableLocationImpl(
+        x = coordinates.x,
+        y = coordinates.y,
+        z = coordinates.z,
         interiorId = interiorId,
         worldId = worldId
 )
@@ -63,6 +105,33 @@ fun angledLocationOf(x: Float, y: Float, z: Float, interiorId: Int, worldId: Int
         angle = angle
 )
 
+fun angledLocationOf(coordinates: Vector3D, interiorId: Int, worldId: Int, angle: Float): AngledLocation = AngledLocationImpl(
+        x = coordinates.x,
+        y = coordinates.y,
+        z = coordinates.z,
+        interiorId = interiorId,
+        worldId = worldId,
+        angle = angle
+)
+
+fun angledLocationOf(location: Location, angle: Float): AngledLocation = AngledLocationImpl(
+        x = location.x,
+        y = location.y,
+        z = location.z,
+        interiorId = location.interiorId,
+        worldId = location.worldId,
+        angle = angle
+)
+
+fun angledLocationOf(position: Position, interiorId: Int, worldId: Int): AngledLocation = AngledLocationImpl(
+        x = position.x,
+        y = position.y,
+        z = position.z,
+        interiorId = interiorId,
+        worldId = worldId,
+        angle = position.angle
+)
+
 fun mutableAngledLocationOf(x: Float, y: Float, z: Float, interiorId: Int, worldId: Int, angle: Float): MutableAngledLocation = MutableAngledLocationImpl(
         x = x,
         y = y,
@@ -70,4 +139,31 @@ fun mutableAngledLocationOf(x: Float, y: Float, z: Float, interiorId: Int, world
         interiorId = interiorId,
         worldId = worldId,
         angle = angle
+)
+
+fun mutableAngledLocationOf(coordinates: Vector3D, interiorId: Int, worldId: Int, angle: Float): MutableAngledLocation = MutableAngledLocationImpl(
+        x = coordinates.x,
+        y = coordinates.y,
+        z = coordinates.z,
+        interiorId = interiorId,
+        worldId = worldId,
+        angle = angle
+)
+
+fun mutableAngledLocationOf(location: Location, angle: Float): MutableAngledLocation = MutableAngledLocationImpl(
+        x = location.x,
+        y = location.y,
+        z = location.z,
+        interiorId = location.interiorId,
+        worldId = location.worldId,
+        angle = angle
+)
+
+fun mutableAngledLocationOf(position: Position, interiorId: Int, worldId: Int): MutableAngledLocation = MutableAngledLocationImpl(
+        x = position.x,
+        y = position.y,
+        z = position.z,
+        interiorId = interiorId,
+        worldId = worldId,
+        angle = position.angle
 )
