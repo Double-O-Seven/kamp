20cdcfe195b1e409f1f5253ffbb8620278ad61fd
leadrian <thewishwithin@gmail.com>
2018-08-01 02:51:09 +0200

    Added Predefined animations

--- kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/data/Animation.kt
+++ kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/data/Animation.kt
@@ -1,6 +1,22 @@
 package ch.leadrian.samp.kamp.api.data
 
-data class Animation(
-        val library: String,
-        val name: String
-)
+interface Animation {
+
+    companion object {
+
+        fun valueOf(library: String, name: String): Animation =
+                AnimationImpl(
+                        library = library,
+                        animationName = name
+                )
+    }
+
+    val library: String
+
+    val animationName: String
+}
+
+private data class AnimationImpl(
+        override val library: String,
+        override val animationName: String
+) : Animation
