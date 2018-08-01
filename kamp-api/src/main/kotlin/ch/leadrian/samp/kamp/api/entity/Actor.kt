b7202c7a921e70f58589beb647e204a5faa599c2
leadrian <thewishwithin@gmail.com>
2018-08-01 03:57:07 +0200

    Implemented Actor interface, Vehicle partially

--- kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/entity/Actor.kt
+++ kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/entity/Actor.kt
@@ -1,8 +1,37 @@
 package ch.leadrian.samp.kamp.api.entity
 
+import ch.leadrian.samp.kamp.api.data.Animation
+import ch.leadrian.samp.kamp.api.data.Position
+import ch.leadrian.samp.kamp.api.data.Vector3D
 import ch.leadrian.samp.kamp.api.entity.id.ActorId
 
 interface Actor : Destroyable {
 
     val id: ActorId
+
+    fun isStreamedIn(forPlayer: Player)
+
+    var virtualWorld: Int
+
+    fun applyAnimation(
+            animation: Animation,
+            fDelta: Float,
+            loop: Boolean,
+            lockX: Boolean,
+            lockY: Boolean,
+            freeze: Boolean,
+            time: Int
+    )
+
+    fun clearAnimation()
+
+    var position3D: Vector3D
+
+    var position: Position
+
+    var angle: Float
+
+    var health: Float
+
+    var isInvulnerable: Boolean
 }
--- kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/entity/Vehicle.kt
+++ kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/entity/Vehicle.kt
@@ -1,4 +1,30 @@
 package ch.leadrian.samp.kamp.api.entity
 
+import ch.leadrian.samp.kamp.api.data.AngledLocation
+import ch.leadrian.samp.kamp.api.data.Location
+import ch.leadrian.samp.kamp.api.data.Position
+import ch.leadrian.samp.kamp.api.data.Vector3D
+import ch.leadrian.samp.kamp.api.entity.id.VehicleId
+
 interface Vehicle : Destroyable {
+
+    val id: VehicleId
+
+    fun isStreamedIn(forPlayer: Player)
+
+    var position3D: Vector3D
+
+    var position: Position
+
+    var location: Location
+
+    var angledLocation: AngledLocation
+
+    var angle: Float
+
+    var interiorId: Int
+
+    var virtualWorld: Int
+
+    // TODO add more
 }
