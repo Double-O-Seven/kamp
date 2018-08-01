a62cf52e43b3fee9da98ccbc598acce3c4fc810d
leadrian <thewishwithin@gmail.com>
2018-08-01 12:38:30 +0200

    Added factory methods for VehicleService and ActorService

--- kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/service/ActorService.kt
+++ kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/service/ActorService.kt
@@ -1,9 +1,21 @@
 package ch.leadrian.samp.kamp.api.service
 
+import ch.leadrian.samp.kamp.api.constants.SkinModel
+import ch.leadrian.samp.kamp.api.data.AngledLocation
+import ch.leadrian.samp.kamp.api.data.Position
+import ch.leadrian.samp.kamp.api.data.Vector3D
 import ch.leadrian.samp.kamp.api.entity.Actor
 import ch.leadrian.samp.kamp.api.entity.id.ActorId
 
 interface ActorService {
+
+    fun isValid(actorId: ActorId): Boolean
+
+    fun createActor(skinModel: SkinModel, position: Position)
+
+    fun createActor(skinModel: SkinModel, coordinates: Vector3D, angle: Float)
+
+    fun createActor(skinModel: SkinModel, angledLocation: AngledLocation)
 
     fun getActor(actorId: ActorId): Actor
 
--- kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/service/VehicleService.kt
+++ kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/service/VehicleService.kt
@@ -1,14 +1,47 @@
 package ch.leadrian.samp.kamp.api.service
 
+import ch.leadrian.samp.kamp.api.constants.VehicleColor
 import ch.leadrian.samp.kamp.api.constants.VehicleModel
 import ch.leadrian.samp.kamp.api.constants.VehicleModelInfoType
+import ch.leadrian.samp.kamp.api.data.AngledLocation
+import ch.leadrian.samp.kamp.api.data.Position
 import ch.leadrian.samp.kamp.api.data.Vector3D
 import ch.leadrian.samp.kamp.api.entity.Vehicle
 import ch.leadrian.samp.kamp.api.entity.id.VehicleId
 
 interface VehicleService {
 
+    fun createVehicle(
+            model: VehicleModel,
+            position: Position,
+            color1: VehicleColor,
+            color: VehicleColor,
+            respawnDelay: Int,
+            addSiren: Boolean = false
+    )
+
+    fun createVehicle(
+            model: VehicleModel,
+            coordinates: Vector3D,
+            rotation: Float,
+            color1: VehicleColor,
+            color: VehicleColor,
+            respawnDelay: Int,
+            addSiren: Boolean = false
+    )
+
+    fun createVehicle(
+            model: VehicleModel,
+            angledLocation: AngledLocation,
+            color1: VehicleColor,
+            color: VehicleColor,
+            respawnDelay: Int,
+            addSiren: Boolean = false
+    )
+
     fun useManualVehicleLightsAndEngine()
+
+    fun isValid(vehicleId: VehicleId): Boolean
 
     fun getVehicle(vehicleId: VehicleId): Vehicle
 
@@ -16,7 +49,7 @@ interface VehicleService {
 
     fun getPoolSize(): Int
 
-    fun enableTirePopping()
+    fun enableTirePopping(enable: Boolean)
 
     fun enableFriendlyFire()
 
