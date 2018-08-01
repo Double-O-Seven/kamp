4b99736ab60e52fd9d48d0837be4bad1195858bb
leadrian <thewishwithin@gmail.com>
2018-08-01 15:34:00 +0200

    Made PlayerCheckpoint and RaceCheckpoint destroyable

--- kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/entity/RaceCheckpoint.kt
+++ kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/entity/RaceCheckpoint.kt
@@ -3,7 +3,7 @@ package ch.leadrian.samp.kamp.api.entity
 import ch.leadrian.samp.kamp.api.constants.RaceCheckpointType
 import ch.leadrian.samp.kamp.api.data.Vector3D
 
-interface RaceCheckpoint {
+interface RaceCheckpoint : Destroyable {
 
     val coordinates: Vector3D
 
