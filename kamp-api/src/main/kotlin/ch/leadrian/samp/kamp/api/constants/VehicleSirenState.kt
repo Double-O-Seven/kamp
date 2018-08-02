3ef1f8791b284c6e62f44fc3ba117ca3451628af
leadrian <thewishwithin@gmail.com>
2018-08-02 19:25:13 +0200

    Added VehicleSirenState

---
+++ kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/constants/VehicleSirenState.kt
@@ -0,0 +1,9 @@
+package ch.leadrian.samp.kamp.api.constants
+
+enum class VehicleSirenState(override val value: Int) : ConstantValue<Int> {
+    ON(1),
+    OFF(0),
+    UNSET(-1);
+
+    companion object : ConstantValueRegistry<Int, VehicleSirenState>(*VehicleSirenState.values())
+}
