a897bd9e1671e0dedc9e1a747732a80ab70ec2cc
leadrian <thewishwithin@gmail.com>
2018-08-05 01:22:17 +0200

    Added code generation for samp native functions wrapper class

--- kamp-codegen/src/main/kotlin/ch/leadrian/samp/kamp/codegen/JavaTypeMappings.kt
+++ kamp-codegen/src/main/kotlin/ch/leadrian/samp/kamp/codegen/JavaTypeMappings.kt
@@ -29,10 +29,31 @@ val JAVA_OUT_TYPE_MAPPING = mapOf(
         Types.STRING to "ch.leadrian.samp.kamp.runtime.types.ReferenceString"
 )
 
+val KOTLIN_TYPE_MAPPING = mapOf(
+        Types.BOOL to "Boolean",
+        Types.CHAR to "Char",
+        Types.FLOAT to "Float",
+        Types.INT to "Int",
+        Types.STRING to "kotlin.String",
+        Types.VOID to "Unit"
+)
+
+val KOTLIN_OUT_TYPE_MAPPING = mapOf(
+        Types.FLOAT to "ch.leadrian.samp.kamp.runtime.types.ReferenceFloat",
+        Types.INT to "ch.leadrian.samp.kamp.runtime.types.ReferenceInt",
+        Types.STRING to "ch.leadrian.samp.kamp.runtime.types.ReferenceString"
+)
+
 fun getJavaType(typeName: String) =
         JAVA_TYPE_MAPPING[typeName] ?: throw IllegalStateException("Unknown Java type: $typeName")
 
 fun getJavaOutType(typeName: String) =
         JAVA_OUT_TYPE_MAPPING[typeName] ?: throw IllegalStateException("Unknown Java out type: $typeName")
+
+fun getKotlinType(typeName: String) =
+        KOTLIN_TYPE_MAPPING[typeName] ?: throw IllegalStateException("Unknown Kotlin type: $typeName")
+
+fun getKotlinOutType(typeName: String) =
+        KOTLIN_OUT_TYPE_MAPPING[typeName] ?: throw IllegalStateException("Unknown Kotlin out type: $typeName")
 
 fun isPrimitiveJavaType(typeName: String): Boolean = PRIMITIVE_JAVA_TYPES.contains(typeName)
---
+++ kamp-codegen/src/main/kotlin/ch/leadrian/samp/kamp/codegen/SAMPNativeFunctionExecutorImplKotlinCodeGenerator.kt
@@ -0,0 +1,84 @@
+package ch.leadrian.samp.kamp.codegen
+
+import ch.leadrian.samp.cidl.model.Function
+import ch.leadrian.samp.cidl.parser.InterfaceDefinitionParser
+import java.io.BufferedWriter
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+import java.time.LocalDateTime
+
+class SAMPNativeFunctionExecutorImplKotlinCodeGenerator {
+
+    fun generate(outputDirectory: Path, functions: List<Function>, packageName: String, className: String = "SAMPNativeFunctionExecutor") {
+        val packageDirectoryPath = outputDirectory.resolve(packageName.replace('.', File.separatorChar))
+        Files.createDirectories(packageDirectoryPath)
+        val outputFile = packageDirectoryPath.resolve("${className}Impl.kt")
+
+        Files.newBufferedWriter(outputFile, StandardOpenOption.CREATE, StandardOpenOption.WRITE).use { writer ->
+            writeHeader(writer, packageName, className)
+            writeFunctions(functions, writer)
+            writeFooter(writer)
+        }
+    }
+
+    private fun writeHeader(writer: BufferedWriter, packageName: String, className: String) {
+        writer.write("""
+            |
+            |package $packageName
+            |
+            |import javax.annotation.Generated
+            |
+            |@Generated(
+            |    value = ["${this::class.java.name}"],
+            |    date = "${LocalDateTime.now()}"
+            |)
+            |object ${className}Impl: $className {
+            |
+            |""".trimMargin("|"))
+    }
+
+    private fun writeFunctions(functions: List<Function>, writer: BufferedWriter) {
+        functions
+                .filter { it.hasAttribute("native") && !it.hasAttribute("noimpl") }
+                .forEach { writeFunction(it, writer) }
+    }
+
+    private fun writeFunction(it: Function, writer: BufferedWriter) {
+        val camelCaseName = "${it.name[0].toLowerCase()}${it.name.substring(1)}"
+        writer.write("    override fun $camelCaseName (")
+        val parameters = it.parameters.joinToString(separator = ", ") {
+            val parameterJavaType = when {
+                it.hasAttribute("out") -> getKotlinOutType(it.type)
+                else -> getKotlinType(it.type)
+            }
+            "${it.name}: $parameterJavaType"
+        }
+        writer.write(parameters)
+        writer.write(") =\n        SAMPNativeFunctions.$camelCaseName(")
+        writer.write(it.parameters.joinToString(separator = ", ") { it.name })
+        writer.write(")\n\n")
+    }
+
+    private fun writeFooter(writer: BufferedWriter) {
+        writer.write("}\n")
+        writer.close()
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val codeGeneratorArguments = CodeGeneratorArguments.parse(args)
+            val interfaceDefinitionParser = InterfaceDefinitionParser()
+            val functions = interfaceDefinitionParser.parse(*codeGeneratorArguments.interfaceDefinitionSources).functions
+            SAMPNativeFunctionExecutorImplKotlinCodeGenerator().generate(
+                    outputDirectory = codeGeneratorArguments.outputDirectoryPath,
+                    functions = functions,
+                    packageName = codeGeneratorArguments.packageName
+            )
+        }
+    }
+
+}
