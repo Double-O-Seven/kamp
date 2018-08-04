020cb367f702a86ef28410d340d583137d532b4b
leadrian <thewishwithin@gmail.com>
2018-08-04 21:18:26 +0200

    Removed formatter args in TextProvider

--- kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/text/TextProvider.kt
+++ kamp-api/src/main/kotlin/ch/leadrian/samp/kamp/api/text/TextProvider.kt
@@ -6,6 +6,4 @@ interface TextProvider {
 
     fun getText(locale: Locale, key: TextKey, defaultText: String? = null)
 
-    fun getText(locale: Locale, key: TextKey, defaultText: String? = null, vararg args: Any)
-
 }
