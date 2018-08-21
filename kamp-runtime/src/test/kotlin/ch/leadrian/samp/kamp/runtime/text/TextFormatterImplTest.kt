3d4720b3a8c87606662d40661b36377b745b91f8
leadrian <thewishwithin@gmail.com>
2018-08-22 01:48:14 +0200

    Removed unused variable

--- kamp-runtime/src/test/kotlin/ch/leadrian/samp/kamp/runtime/text/TextFormatterImplTest.kt
+++ kamp-runtime/src/test/kotlin/ch/leadrian/samp/kamp/runtime/text/TextFormatterImplTest.kt
@@ -1,6 +1,5 @@
 package ch.leadrian.samp.kamp.runtime.text
 
-import ch.leadrian.samp.kamp.api.data.COLOR_RED
 import ch.leadrian.samp.kamp.api.text.HasTextKey
 import ch.leadrian.samp.kamp.api.text.TextKey
 import ch.leadrian.samp.kamp.api.text.TextProvider
@@ -15,7 +14,6 @@ internal class TextFormatterImplTest {
     @Test
     fun shouldFormatText() {
         val locale = Locale.GERMANY
-        val color = COLOR_RED
         val textKey1 = TextKey("test.key.abc")
         val textKey2 = TextKey("test.key.def")
         val hasTextKey = mockk<HasTextKey> {
