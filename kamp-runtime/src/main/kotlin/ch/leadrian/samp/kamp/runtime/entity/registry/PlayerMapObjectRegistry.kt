c3a7aa3ca45662516f664514052dddc36cf6aa67
leadrian <thewishwithin@gmail.com>
2018-09-05 22:41:52 +0200

    Removed @Singleton from PlayerMapObjectRegistry

--- kamp-runtime/src/main/kotlin/ch/leadrian/samp/kamp/runtime/entity/registry/PlayerMapObjectRegistry.kt
+++ kamp-runtime/src/main/kotlin/ch/leadrian/samp/kamp/runtime/entity/registry/PlayerMapObjectRegistry.kt
@@ -3,7 +3,5 @@ package ch.leadrian.samp.kamp.runtime.entity.registry
 import ch.leadrian.samp.kamp.api.constants.SAMPConstants
 import ch.leadrian.samp.kamp.api.entity.id.PlayerMapObjectId
 import ch.leadrian.samp.kamp.runtime.entity.PlayerMapObjectImpl
-import javax.inject.Singleton
 
-@Singleton
 internal class PlayerMapObjectRegistry : EntityRegistry<PlayerMapObjectImpl, PlayerMapObjectId>(arrayOfNulls(SAMPConstants.MAX_OBJECTS))
