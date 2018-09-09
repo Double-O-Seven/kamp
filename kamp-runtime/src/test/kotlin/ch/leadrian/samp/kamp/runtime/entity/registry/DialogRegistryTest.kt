0a50711d7638c0347ce4a9880337afb9f04ce7c0
leadrian <thewishwithin@gmail.com>
2018-09-09 23:11:43 +0200

    Added DialogRegistry

---
+++ kamp-runtime/src/test/kotlin/ch/leadrian/samp/kamp/runtime/entity/registry/DialogRegistryTest.kt
@@ -0,0 +1,96 @@
+package ch.leadrian.samp.kamp.runtime.entity.registry
+
+import ch.leadrian.samp.kamp.api.entity.Dialog
+import ch.leadrian.samp.kamp.api.entity.Player
+import ch.leadrian.samp.kamp.api.entity.id.DialogId
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
+import org.junit.jupiter.api.Test
+
+internal class DialogRegistryTest {
+
+    @Test
+    fun shouldAllocateDialogIdForSingleDialog() {
+        val dialog = TestDialog()
+        val dialogRegistry = DialogRegistry(3)
+
+        dialogRegistry.register { dialog.apply { id = it } }
+
+        assertThat(dialog.id)
+                .isEqualTo(DialogId.valueOf(0))
+    }
+
+    @Test
+    fun shouldRegisterSingleDialog() {
+        val dialog = TestDialog()
+        val dialogRegistry = DialogRegistry(3)
+
+        dialogRegistry.register { dialog.apply { id = it } }
+
+        assertThat(dialogRegistry[DialogId.valueOf(0)])
+                .isEqualTo(dialog)
+    }
+
+    @Test
+    fun shouldAllocateDialogIdForMultipleDialogs() {
+        val dialog1 = TestDialog()
+        val dialog2 = TestDialog()
+        val dialog3 = TestDialog()
+        val dialogRegistry = DialogRegistry(3)
+
+        dialogRegistry.register { dialog1.apply { id = it } }
+        dialogRegistry.register { dialog2.apply { id = it } }
+        dialogRegistry.register { dialog3.apply { id = it } }
+
+        assertThat(dialog1.id)
+                .isEqualTo(DialogId.valueOf(0))
+        assertThat(dialog2.id)
+                .isEqualTo(DialogId.valueOf(1))
+        assertThat(dialog3.id)
+                .isEqualTo(DialogId.valueOf(2))
+    }
+
+    @Test
+    fun shouldRegisterMultipleDialogs() {
+        val dialog1 = TestDialog()
+        val dialog2 = TestDialog()
+        val dialog3 = TestDialog()
+        val dialogRegistry = DialogRegistry(3)
+
+        dialogRegistry.register { dialog1.apply { id = it } }
+        dialogRegistry.register { dialog2.apply { id = it } }
+        dialogRegistry.register { dialog3.apply { id = it } }
+
+        assertThat(dialogRegistry[DialogId.valueOf(0)])
+                .isEqualTo(dialog1)
+        assertThat(dialogRegistry[DialogId.valueOf(1)])
+                .isEqualTo(dialog2)
+        assertThat(dialogRegistry[DialogId.valueOf(2)])
+                .isEqualTo(dialog3)
+    }
+
+    @Test
+    fun givenCapacityIsReachedItShouldThrowAnException() {
+        val dialog1 = TestDialog()
+        val dialog2 = TestDialog()
+        val dialog3 = TestDialog()
+        val dialogRegistry = DialogRegistry(2)
+
+        dialogRegistry.register { dialog1 }
+        dialogRegistry.register { dialog2 }
+
+        val caughtThrowable = catchThrowable { dialogRegistry.register { dialog3 } }
+
+        assertThat(caughtThrowable)
+                .isInstanceOf(NoSuchElementException::class.java)
+    }
+
+    private class TestDialog : Dialog {
+
+        override lateinit var id: DialogId
+
+        override fun show(forPlayer: Player) = throw UnsupportedOperationException()
+
+    }
+
+}
