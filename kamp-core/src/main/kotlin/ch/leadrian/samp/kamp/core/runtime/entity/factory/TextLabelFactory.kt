538addc7902228a3d2f645c8a0eef4eb16e14ed9
leadrian <thewishwithin@gmail.com>
2018-09-11 23:28:07 +0200

    Added TextLabelFactory

---
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/runtime/entity/factory/TextLabelFactory.kt
@@ -0,0 +1,43 @@
+package ch.leadrian.samp.kamp.core.runtime.entity.factory
+
+import ch.leadrian.samp.kamp.core.api.data.Color
+import ch.leadrian.samp.kamp.core.api.data.Vector3D
+import ch.leadrian.samp.kamp.core.api.entity.TextLabel
+import ch.leadrian.samp.kamp.core.api.text.TextFormatter
+import ch.leadrian.samp.kamp.core.api.text.TextProvider
+import ch.leadrian.samp.kamp.core.runtime.SAMPNativeFunctionExecutor
+import ch.leadrian.samp.kamp.core.runtime.entity.registry.TextLabelRegistry
+import javax.inject.Inject
+
+internal class TextLabelFactory
+@Inject
+constructor(
+        private val nativeFunctionExecutor: SAMPNativeFunctionExecutor,
+        private val textLabelRegistry: TextLabelRegistry,
+        private val textProvider: TextProvider,
+        private val textFormatter: TextFormatter
+) {
+
+    fun create(
+            coordinates: Vector3D,
+            virtualWorldId: Int,
+            text: String,
+            color: Color,
+            drawDistance: Float,
+            testLOS: Boolean
+    ): TextLabel {
+        val textLabel = TextLabel(
+                coordinates = coordinates,
+                text = text,
+                color = color,
+                drawDistance = drawDistance,
+                testLOS = testLOS,
+                nativeFunctionExecutor = nativeFunctionExecutor,
+                virtualWorldId = virtualWorldId
+        )
+        textLabelRegistry.register(textLabel)
+        textLabel.onDestroy { textLabelRegistry.unregister(this) }
+        return textLabel
+    }
+
+}
---
+++ kamp-core/src/test/kotlin/ch/leadrian/samp/kamp/core/runtime/entity/factory/TextLabelFactoryTest.kt
@@ -0,0 +1,93 @@
+package ch.leadrian.samp.kamp.core.runtime.entity.factory
+
+import ch.leadrian.samp.kamp.core.api.data.Colors
+import ch.leadrian.samp.kamp.core.api.data.vector3DOf
+import ch.leadrian.samp.kamp.core.api.entity.TextLabel
+import ch.leadrian.samp.kamp.core.api.text.TextFormatter
+import ch.leadrian.samp.kamp.core.api.text.TextProvider
+import ch.leadrian.samp.kamp.core.runtime.SAMPNativeFunctionExecutor
+import ch.leadrian.samp.kamp.core.runtime.entity.registry.TextLabelRegistry
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class TextLabelFactoryTest {
+
+    private lateinit var textLabelFactory: TextLabelFactory
+
+    private val textLabelRegistry = mockk<TextLabelRegistry>()
+    private val nativeFunctionExecutor = mockk<SAMPNativeFunctionExecutor>()
+    private val textProvider = mockk<TextProvider>()
+    private val textFormatter = mockk<TextFormatter>()
+
+    @BeforeEach
+    fun setUp() {
+        every { nativeFunctionExecutor.create3DTextLabel(any(), any(), any(), any(), any(), any(), any(), any()) } returns 0
+        every { textLabelRegistry.register(any()) } just Runs
+        every { textLabelRegistry.unregister(any()) } just Runs
+        textLabelFactory = TextLabelFactory(nativeFunctionExecutor, textLabelRegistry, textProvider, textFormatter)
+    }
+
+    @Test
+    fun shouldCreateTextLabel() {
+        textLabelFactory.create(
+                coordinates = vector3DOf(x = 1f, y = 2f, z = 3f),
+                text = "Test",
+                color = Colors.RED,
+                testLOS = true,
+                drawDistance = 4f,
+                virtualWorldId = 69
+        )
+
+        verify {
+            nativeFunctionExecutor.create3DTextLabel(
+                    x = 1f,
+                    y = 2f,
+                    z = 3f,
+                    text = "Test",
+                    testLOS = true,
+                    color = Colors.RED.value,
+                    DrawDistance = 4f,
+                    virtualworld = 69
+            )
+        }
+    }
+
+    @Test
+    fun shouldRegisterTextLabel() {
+        val textLabel = textLabelFactory.create(
+                coordinates = vector3DOf(x = 1f, y = 2f, z = 3f),
+                text = "Test",
+                color = Colors.RED,
+                testLOS = true,
+                drawDistance = 4f,
+                virtualWorldId = 69
+        )
+
+        verify { textLabelRegistry.register(textLabel) }
+    }
+
+    @Test
+    fun shouldUnregisterTextLabelOnDestroy() {
+        every { nativeFunctionExecutor.delete3DTextLabel(any()) } returns true
+        val textLabel = textLabelFactory.create(
+                coordinates = vector3DOf(x = 1f, y = 2f, z = 3f),
+                text = "Test",
+                color = Colors.RED,
+                testLOS = true,
+                drawDistance = 4f,
+                virtualWorldId = 69
+        )
+        val onDestroy = mockk<TextLabel.() -> Unit>(relaxed = true)
+        textLabel.onDestroy(onDestroy)
+
+        textLabel.destroy()
+
+        verify { onDestroy.invoke(textLabel) }
+    }
+
+}
