477579922fb7a3d2fcb308a87cb25fa2f7c99721
leadrian <thewishwithin@gmail.com>
2018-09-11 23:14:51 +0200

    Added TextDrawFactory

---
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/runtime/entity/factory/TextDrawFactory.kt
@@ -0,0 +1,35 @@
+package ch.leadrian.samp.kamp.core.runtime.entity.factory
+
+import ch.leadrian.samp.kamp.core.api.data.Vector2D
+import ch.leadrian.samp.kamp.core.api.entity.TextDraw
+import ch.leadrian.samp.kamp.core.api.text.TextFormatter
+import ch.leadrian.samp.kamp.core.api.text.TextProvider
+import ch.leadrian.samp.kamp.core.runtime.SAMPNativeFunctionExecutor
+import ch.leadrian.samp.kamp.core.runtime.entity.registry.TextDrawRegistry
+import java.util.*
+import javax.inject.Inject
+
+internal class TextDrawFactory
+@Inject
+constructor(
+        private val nativeFunctionExecutor: SAMPNativeFunctionExecutor,
+        private val textDrawRegistry: TextDrawRegistry,
+        private val textProvider: TextProvider,
+        private val textFormatter: TextFormatter
+) {
+
+    fun create(text: String, position: Vector2D, locale: Locale): TextDraw {
+        val textDraw = TextDraw(
+                text = text,
+                position = position,
+                nativeFunctionExecutor = nativeFunctionExecutor,
+                textProvider = textProvider,
+                textFormatter = textFormatter,
+                locale = locale
+        )
+        textDrawRegistry.register(textDraw)
+        textDraw.onDestroy { textDrawRegistry.unregister(this) }
+        return textDraw
+    }
+
+}
---
+++ kamp-core/src/test/kotlin/ch/leadrian/samp/kamp/core/runtime/entity/factory/TextDrawFactoryTest.kt
@@ -0,0 +1,80 @@
+package ch.leadrian.samp.kamp.core.runtime.entity.factory
+
+import ch.leadrian.samp.kamp.core.api.data.vector2DOf
+import ch.leadrian.samp.kamp.core.api.entity.TextDraw
+import ch.leadrian.samp.kamp.core.api.text.TextFormatter
+import ch.leadrian.samp.kamp.core.api.text.TextProvider
+import ch.leadrian.samp.kamp.core.runtime.SAMPNativeFunctionExecutor
+import ch.leadrian.samp.kamp.core.runtime.entity.registry.TextDrawRegistry
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.*
+
+internal class TextDrawFactoryTest {
+
+    private val locale = Locale.GERMANY
+    private lateinit var textDrawFactory: TextDrawFactory
+
+    private val textDrawRegistry = mockk<TextDrawRegistry>()
+    private val nativeFunctionExecutor = mockk<SAMPNativeFunctionExecutor>()
+    private val textProvider = mockk<TextProvider>()
+    private val textFormatter = mockk<TextFormatter>()
+
+    @BeforeEach
+    fun setUp() {
+        every { nativeFunctionExecutor.textDrawCreate(any(), any(), any()) } returns 0
+        every { textDrawRegistry.register(any()) } just Runs
+        every { textDrawRegistry.unregister(any()) } just Runs
+        textDrawFactory = TextDrawFactory(nativeFunctionExecutor, textDrawRegistry, textProvider, textFormatter)
+    }
+
+    @Test
+    fun shouldCreateTextDraw() {
+        textDrawFactory.create(
+                position = vector2DOf(x = 1f, y = 2f),
+                text = "Test",
+                locale = locale
+        )
+
+        verify {
+            nativeFunctionExecutor.textDrawCreate(
+                    x = 1f,
+                    y = 2f,
+                    text = "Test"
+            )
+        }
+    }
+
+    @Test
+    fun shouldRegisterTextDraw() {
+        val textDraw = textDrawFactory.create(
+                position = vector2DOf(x = 1f, y = 2f),
+                text = "Test",
+                locale = locale
+        )
+
+        verify { textDrawRegistry.register(textDraw) }
+    }
+
+    @Test
+    fun shouldUnregisterTextDrawOnDestroy() {
+        every { nativeFunctionExecutor.textDrawDestroy(any()) } returns true
+        val textDraw = textDrawFactory.create(
+                position = vector2DOf(x = 1f, y = 2f),
+                text = "Test",
+                locale = locale
+        )
+        val onDestroy = mockk<TextDraw.() -> Unit>(relaxed = true)
+        textDraw.onDestroy(onDestroy)
+
+        textDraw.destroy()
+
+        verify { onDestroy.invoke(textDraw) }
+    }
+
+}
