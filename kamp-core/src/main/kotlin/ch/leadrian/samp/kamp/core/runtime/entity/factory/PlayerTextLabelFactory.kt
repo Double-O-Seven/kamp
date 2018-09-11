ee9d5d81482e531153a7abb66dd1a2d9e2211b6b
leadrian <thewishwithin@gmail.com>
2018-09-11 23:23:29 +0200

    Added PlayerTextLabelFactory

---
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/runtime/entity/factory/PlayerTextLabelFactory.kt
@@ -0,0 +1,47 @@
+package ch.leadrian.samp.kamp.core.runtime.entity.factory
+
+import ch.leadrian.samp.kamp.core.api.data.Color
+import ch.leadrian.samp.kamp.core.api.data.Vector3D
+import ch.leadrian.samp.kamp.core.api.entity.Player
+import ch.leadrian.samp.kamp.core.api.entity.PlayerTextLabel
+import ch.leadrian.samp.kamp.core.api.entity.Vehicle
+import ch.leadrian.samp.kamp.core.api.text.TextFormatter
+import ch.leadrian.samp.kamp.core.api.text.TextProvider
+import ch.leadrian.samp.kamp.core.runtime.SAMPNativeFunctionExecutor
+import javax.inject.Inject
+
+internal class PlayerTextLabelFactory
+@Inject
+constructor(
+        private val nativeFunctionExecutor: SAMPNativeFunctionExecutor,
+        private val textProvider: TextProvider,
+        private val textFormatter: TextFormatter
+) {
+
+    fun create(
+            player: Player,
+            coordinates: Vector3D,
+            text: String,
+            color: Color,
+            drawDistance: Float,
+            testLOS: Boolean,
+            attachToPlayer: Player? = null,
+            attachToVehicle: Vehicle? = null
+    ): PlayerTextLabel {
+        val playerTextLabel = PlayerTextLabel(
+                player = player,
+                coordinates = coordinates,
+                text = text,
+                color = color,
+                drawDistance = drawDistance,
+                testLOS = testLOS,
+                nativeFunctionExecutor = nativeFunctionExecutor,
+                attachToPlayer = attachToPlayer,
+                attachToVehicle = attachToVehicle
+        )
+        player.playerTextLabelRegistry.register(playerTextLabel)
+        playerTextLabel.onDestroy { player.playerTextLabelRegistry.unregister(this) }
+        return playerTextLabel
+    }
+
+}
---
+++ kamp-core/src/test/kotlin/ch/leadrian/samp/kamp/core/runtime/entity/factory/PlayerTextLabelFactoryTest.kt
@@ -0,0 +1,117 @@
+package ch.leadrian.samp.kamp.core.runtime.entity.factory
+
+import ch.leadrian.samp.kamp.core.api.data.Colors
+import ch.leadrian.samp.kamp.core.api.data.vector3DOf
+import ch.leadrian.samp.kamp.core.api.entity.Player
+import ch.leadrian.samp.kamp.core.api.entity.PlayerTextLabel
+import ch.leadrian.samp.kamp.core.api.entity.Vehicle
+import ch.leadrian.samp.kamp.core.api.entity.id.PlayerId
+import ch.leadrian.samp.kamp.core.api.entity.id.VehicleId
+import ch.leadrian.samp.kamp.core.api.text.TextFormatter
+import ch.leadrian.samp.kamp.core.api.text.TextProvider
+import ch.leadrian.samp.kamp.core.runtime.SAMPNativeFunctionExecutor
+import ch.leadrian.samp.kamp.core.runtime.entity.registry.PlayerTextLabelRegistry
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class PlayerTextLabelFactoryTest {
+
+    private val attachToPlayerId = PlayerId.valueOf(50)
+    private val attachToPlayer = mockk<Player>()
+    private val attachToVehicleId = VehicleId.valueOf(187)
+    private val attachToVehicle = mockk<Vehicle>()
+    private val playerId = PlayerId.valueOf(69)
+    private lateinit var player: Player
+    private lateinit var playerTextLabelFactory: PlayerTextLabelFactory
+
+    private val playerTextLabelRegistry = mockk<PlayerTextLabelRegistry>()
+    private val nativeFunctionExecutor = mockk<SAMPNativeFunctionExecutor>()
+    private val textProvider = mockk<TextProvider>()
+    private val textFormatter = mockk<TextFormatter>()
+
+    @BeforeEach
+    fun setUp() {
+        every { attachToPlayer.id } returns attachToPlayerId
+        every { attachToVehicle.id } returns attachToVehicleId
+        every { nativeFunctionExecutor.createPlayer3DTextLabel(any(), any(), any(), any(), any(), any(), any(), any(), any(), any()) } returns 0
+        every { playerTextLabelRegistry.register(any()) } just Runs
+        every { playerTextLabelRegistry.unregister(any()) } just Runs
+        playerTextLabelFactory = PlayerTextLabelFactory(nativeFunctionExecutor, textProvider, textFormatter)
+        player = mockk {
+            every { this@mockk.playerTextLabelRegistry } returns this@PlayerTextLabelFactoryTest.playerTextLabelRegistry
+            every { id } returns playerId
+        }
+    }
+
+    @Test
+    fun shouldCreatePlayerTextLabel() {
+        playerTextLabelFactory.create(
+                player = player,
+                coordinates = vector3DOf(x = 1f, y = 2f, z = 3f),
+                text = "Test",
+                color = Colors.RED,
+                testLOS = true,
+                drawDistance = 4f,
+                attachToPlayer = attachToPlayer,
+                attachToVehicle = attachToVehicle
+        )
+
+        verify {
+            nativeFunctionExecutor.createPlayer3DTextLabel(
+                    playerid = playerId.value,
+                    x = 1f,
+                    y = 2f,
+                    z = 3f,
+                    text = "Test",
+                    testLOS = true,
+                    color = Colors.RED.value,
+                    DrawDistance = 4f,
+                    attachedplayer = attachToPlayerId.value,
+                    attachedvehicle = attachToVehicleId.value
+            )
+        }
+    }
+
+    @Test
+    fun shouldRegisterPlayerTextLabel() {
+        val playerTextLabel = playerTextLabelFactory.create(
+                player = player,
+                coordinates = vector3DOf(x = 1f, y = 2f, z = 3f),
+                text = "Test",
+                color = Colors.RED,
+                testLOS = true,
+                drawDistance = 4f,
+                attachToPlayer = attachToPlayer,
+                attachToVehicle = attachToVehicle
+        )
+
+        verify { playerTextLabelRegistry.register(playerTextLabel) }
+    }
+
+    @Test
+    fun shouldUnregisterPlayerTextLabelOnDestroy() {
+        every { nativeFunctionExecutor.deletePlayer3DTextLabel(any(), any()) } returns true
+        val playerTextLabel = playerTextLabelFactory.create(
+                player = player,
+                coordinates = vector3DOf(x = 1f, y = 2f, z = 3f),
+                text = "Test",
+                color = Colors.RED,
+                testLOS = true,
+                drawDistance = 4f,
+                attachToPlayer = attachToPlayer,
+                attachToVehicle = attachToVehicle
+        )
+        val onDestroy = mockk<PlayerTextLabel.() -> Unit>(relaxed = true)
+        playerTextLabel.onDestroy(onDestroy)
+
+        playerTextLabel.destroy()
+
+        verify { onDestroy.invoke(playerTextLabel) }
+    }
+
+}
