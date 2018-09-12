841a4eb41a5cbabf486fcfa100b7194307dbfd13
leadrian <thewishwithin@gmail.com>
2018-09-13 00:22:22 +0200

    Fixed CommandAccessChecker signature

--- kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/command/CommandAccessChecker.kt
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/command/CommandAccessChecker.kt
@@ -1,10 +1,9 @@
 package ch.leadrian.samp.kamp.core.api.command
 
 import ch.leadrian.samp.kamp.core.api.entity.Player
-import java.lang.reflect.Method
 
 interface CommandAccessChecker {
 
-    fun isAccessGranted(player: Player, command: String, parameters: List<String>, method: Method): Boolean
+    fun isAccessGranted(player: Player, commandDefinition: CommandDefinition, parameters: List<String>): Boolean
 
 }
