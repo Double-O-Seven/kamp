607d45c2d481b992f9e3ab5ba7348b38f3dbcf56
leadrian <thewishwithin@gmail.com>
2018-09-13 19:43:44 +0200

    Added DefaultCommandErrorHandler

---
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/command/DefaultCommandErrorHandler.kt
@@ -0,0 +1,32 @@
+package ch.leadrian.samp.kamp.core.api.command
+
+import ch.leadrian.samp.kamp.core.TextKeys
+import ch.leadrian.samp.kamp.core.api.callback.OnPlayerCommandTextListener
+import ch.leadrian.samp.kamp.core.api.data.Colors
+import ch.leadrian.samp.kamp.core.api.entity.Player
+import ch.leadrian.samp.kamp.core.api.text.MessageSender
+import ch.leadrian.samp.kamp.core.api.text.TextProvider
+import javax.inject.Inject
+
+class DefaultCommandErrorHandler
+@Inject
+constructor(
+        private val messageSender: MessageSender,
+        private val textProvider: TextProvider
+) : CommandErrorHandler {
+
+    override fun handle(player: Player, commandDefinition: CommandDefinition, parameters: List<String>): OnPlayerCommandTextListener.Result {
+        val message = StringBuilder().apply {
+            append(textProvider.getText(player.locale, TextKeys.command.usage.prefix))
+            append(": /")
+            append(commandDefinition.name)
+            commandDefinition.parameters.forEach {
+                append(" [")
+                append(it.getName(player.locale))
+                append(']')
+            }
+        }.toString()
+        messageSender.sendMessageToPlayer(player, Colors.RED, message)
+        return OnPlayerCommandTextListener.Result.Processed
+    }
+}
---
+++ kamp-core/src/test/kotlin/ch/leadrian/samp/kamp/core/api/command/DefaultCommandErrorHandlerTest.kt
@@ -0,0 +1,52 @@
+package ch.leadrian.samp.kamp.core.api.command
+
+import ch.leadrian.samp.kamp.core.TextKeys
+import ch.leadrian.samp.kamp.core.api.data.Colors
+import ch.leadrian.samp.kamp.core.api.entity.Player
+import ch.leadrian.samp.kamp.core.api.text.MessageSender
+import ch.leadrian.samp.kamp.core.api.text.TextProvider
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import java.util.*
+
+internal class DefaultCommandErrorHandlerTest {
+
+    @Test
+    fun shouldSendMessageToPlayer() {
+        val locale = Locale.GERMANY
+        val player = mockk<Player> {
+            every { this@mockk.locale } returns locale
+        }
+        val parameter1 = mockk<CommandParameterDefinition> {
+            every { getName(locale) } returns "Player ID"
+        }
+        val parameter2 = mockk<CommandParameterDefinition> {
+            every { getName(locale) } returns "Message"
+        }
+        val command = mockk<CommandDefinition> {
+            every { name } returns "pm"
+            every { parameters } returns listOf(parameter1, parameter2)
+        }
+        val textProvider = mockk<TextProvider> {
+            every { getText(locale, TextKeys.command.usage.prefix) } returns "Usage"
+        }
+        val messageSender = mockk<MessageSender> {
+            every { sendMessageToPlayer(any(), any(), any<String>()) } just Runs
+        }
+        val defaultUnknownCommandHandler = DefaultCommandErrorHandler(
+                textProvider = textProvider,
+                messageSender = messageSender
+        )
+
+        defaultUnknownCommandHandler.handle(player, command, emptyList())
+
+        verify {
+            messageSender.sendMessageToPlayer(player, Colors.RED, "Usage: /pm [Player ID] [Message]")
+        }
+    }
+
+}
