9fae0f79ab4a13b5fd4f3ba85a4e58df28b137f3
leadrian <thewishwithin@gmail.com>
2018-09-17 00:17:37 +0200

    Made ParsedCommand an internal class

--- kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/runtime/command/ParsedCommand.kt
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/runtime/command/ParsedCommand.kt
@@ -1,6 +1,6 @@
 package ch.leadrian.samp.kamp.core.runtime.command
 
-data class ParsedCommand(
+internal data class ParsedCommand(
         val command: String,
         val parameterValues: List<String> = emptyList()
 )
