63d683c0cd27a9ea006f168cba0cf5b4fc2151ac
leadrian <thewishwithin@gmail.com>
2018-09-19 21:45:20 +0200

    Implemented CommandProcessor

--- kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/runtime/command/CommandProcessor.kt
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/runtime/command/CommandProcessor.kt
@@ -1,16 +1,85 @@
 package ch.leadrian.samp.kamp.core.runtime.command
 
 import ch.leadrian.samp.kamp.core.api.callback.OnPlayerCommandTextListener
+import ch.leadrian.samp.kamp.core.api.command.CommandDefinition
+import ch.leadrian.samp.kamp.core.api.command.CommandErrorHandler
+import ch.leadrian.samp.kamp.core.api.command.DefaultCommandErrorHandler
+import ch.leadrian.samp.kamp.core.api.command.DefaultUnknownCommandHandler
+import ch.leadrian.samp.kamp.core.api.command.UnknownCommandHandler
 import ch.leadrian.samp.kamp.core.api.entity.Player
+import ch.leadrian.samp.kamp.core.api.util.loggerFor
+import javax.annotation.PostConstruct
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 internal class CommandProcessor
-@Inject constructor() : OnPlayerCommandTextListener {
+@Inject
+constructor(
+        private val commandParser: CommandParser,
+        private val commandRegistry: CommandRegistry,
+        private val commandAccessCheckExecutor: CommandAccessCheckExecutor,
+        private val commandParametersResolver: CommandParametersResolver,
+        private val commandExecutor: CommandExecutor,
+        defaultUnknownCommandHandler: DefaultUnknownCommandHandler,
+        defaultCommandErrorHandler: DefaultCommandErrorHandler
+) : OnPlayerCommandTextListener {
+
+    private companion object {
+
+        private val log = loggerFor<CommandProcessor>()
+
+    }
+
+    @field:com.google.inject.Inject(optional = true)
+    private var unknownCommandHandler: UnknownCommandHandler = defaultUnknownCommandHandler
+
+    @field:com.google.inject.Inject(optional = true)
+    private var commandErrorHandler: CommandErrorHandler = defaultCommandErrorHandler
+
+    @PostConstruct
+    fun initialize() {
+        TODO("register as callback listener")
+    }
 
     override fun onPlayerCommandText(player: Player, commandText: String): OnPlayerCommandTextListener.Result {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        try {
+            val parsedCommand = commandParser.parse(commandText)
+                    ?: return commandErrorHandler.handle(player, commandText, null)
+
+            val commandDefinition = getCommandDefinition(parsedCommand)
+                    ?: return unknownCommandHandler.handle(player, parsedCommand.command, parsedCommand.parameterValues)
+
+            val stringParameterValues = getStringParameterValues(commandDefinition, parsedCommand)
+
+            return processCommand(player, commandDefinition, stringParameterValues)
+        } catch (e: Exception) {
+            log.error("Exception while processing command by player ${player.name}: $commandText", e)
+            return commandErrorHandler.handle(player, commandText, e)
+        }
     }
+
+    private fun getCommandDefinition(parsedCommand: ParsedCommand) =
+            commandRegistry.getCommandDefinition(parsedCommand.command, parsedCommand.parameterValues.firstOrNull())
+
+    private fun getStringParameterValues(commandDefinition: CommandDefinition, parsedCommand: ParsedCommand): List<String> =
+            when {
+                commandDefinition.groupName == null -> parsedCommand.parameterValues
+                else -> parsedCommand.parameterValues.drop(1)
+            }
+
+    private fun processCommand(
+            player: Player,
+            commandDefinition: CommandDefinition,
+            stringParameterValues: List<String>
+    ): OnPlayerCommandTextListener.Result {
+        commandAccessCheckExecutor.checkAccess(player, commandDefinition, stringParameterValues)?.let { return it }
+        val result = commandParametersResolver.resolve(player, commandDefinition, stringParameterValues)
+        return when (result) {
+            is CommandParametersResolver.Result.ParameterValues -> commandExecutor.execute(commandDefinition, result.parameterValues)
+            is CommandParametersResolver.Result.Error -> result.returnValue
+        }
+    }
+
 
 }
---
+++ kamp-core/src/test/kotlin/ch/leadrian/samp/kamp/core/runtime/command/CommandProcessorTest.kt
@@ -0,0 +1,309 @@
+package ch.leadrian.samp.kamp.core.runtime.command
+
+import ch.leadrian.samp.kamp.core.api.callback.OnPlayerCommandTextListener
+import ch.leadrian.samp.kamp.core.api.command.CommandDefinition
+import ch.leadrian.samp.kamp.core.api.command.Commands
+import ch.leadrian.samp.kamp.core.api.command.DefaultCommandErrorHandler
+import ch.leadrian.samp.kamp.core.api.command.DefaultUnknownCommandHandler
+import ch.leadrian.samp.kamp.core.api.entity.Player
+import io.mockk.Called
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class CommandProcessorTest {
+
+    private lateinit var commandProcessor: CommandProcessor
+
+    private val commandParser = mockk<CommandParser>()
+    private val commandRegistry = mockk<CommandRegistry>()
+    private val commandAccessCheckExecutor = mockk<CommandAccessCheckExecutor>()
+    private val commandParametersResolver = mockk<CommandParametersResolver>()
+    private val commandExecutor = mockk<CommandExecutor>()
+    private val unknownCommandHandler = mockk<DefaultUnknownCommandHandler>()
+    private val defaultCommandErrorHandler = mockk<DefaultCommandErrorHandler>()
+    private val player = mockk<Player>()
+    private val method = Any::class.java.getMethod("hashCode")
+
+    @BeforeEach
+    fun setUp() {
+        commandProcessor = CommandProcessor(
+                commandParser = commandParser,
+                commandRegistry = commandRegistry,
+                commandAccessCheckExecutor = commandAccessCheckExecutor,
+                commandParametersResolver = commandParametersResolver,
+                commandExecutor = commandExecutor,
+                defaultUnknownCommandHandler = unknownCommandHandler,
+                defaultCommandErrorHandler = defaultCommandErrorHandler
+        )
+    }
+
+    @Test
+    fun givenCommandCouldBeParsedItShouldExecuteCommandErrorHandler() {
+        every {
+            defaultCommandErrorHandler.handle(any(), any(), any())
+        } returns OnPlayerCommandTextListener.Result.Processed
+        val commandLine = "/hi there"
+        every { commandParser.parse(commandLine) } returns null
+
+        commandProcessor.onPlayerCommandText(player, commandLine)
+
+        verify {
+            defaultCommandErrorHandler.handle(player, commandLine, null)
+        }
+    }
+
+    @Test
+    fun givenCommandDefinitionCouldNotBeFoundItShouldExecuteUnknownCommandHandler() {
+        val commandLine = "/hi there"
+        every { commandParser.parse(commandLine) } returns ParsedCommand("hi", listOf("there", "lol"))
+        every { commandRegistry.getCommandDefinition("hi", "there") } returns null
+        every {
+            unknownCommandHandler.handle(any(), any(), any())
+        } returns OnPlayerCommandTextListener.Result.Processed
+
+        commandProcessor.onPlayerCommandText(player, commandLine)
+
+        verify { unknownCommandHandler.handle(player, "hi", listOf("there", "lol")) }
+    }
+
+    @Nested
+    inner class CommandWithGroupTests {
+
+        @Test
+        fun givenAccessIsDeniedItShouldReturn() {
+            val commandLine = "/hi there"
+            val commandDefinition = CommandDefinition(
+                    name = "hi",
+                    groupName = "there",
+                    method = method,
+                    commandsInstance = TestCommands,
+                    parameters = listOf()
+            )
+            every { commandParser.parse(commandLine) } returns ParsedCommand("hi", listOf("there", "lol"))
+            every { commandRegistry.getCommandDefinition("hi", "there") } returns commandDefinition
+            every {
+                commandAccessCheckExecutor.checkAccess(player, commandDefinition, listOf("lol"))
+            } returns OnPlayerCommandTextListener.Result.Processed
+
+            val result = commandProcessor.onPlayerCommandText(player, commandLine)
+
+            assertThat(result)
+                    .isEqualTo(OnPlayerCommandTextListener.Result.Processed)
+        }
+
+        @Test
+        fun givenParametersAreResolvedItShouldExecuteCommands() {
+            val commandLine = "/hi there"
+            val commandDefinition = CommandDefinition(
+                    name = "hi",
+                    groupName = "there",
+                    method = method,
+                    commandsInstance = TestCommands,
+                    parameters = listOf()
+            )
+            every { commandParser.parse(commandLine) } returns ParsedCommand("hi", listOf("there", "123"))
+            every { commandRegistry.getCommandDefinition("hi", "there") } returns commandDefinition
+            every {
+                commandAccessCheckExecutor.checkAccess(player, commandDefinition, listOf("123"))
+            } returns null
+            every {
+                commandParametersResolver.resolve(player, commandDefinition, listOf("123"))
+            } returns CommandParametersResolver.Result.ParameterValues(arrayOf(player, 123))
+            every { commandExecutor.execute(any(), any()) } returns OnPlayerCommandTextListener.Result.Processed
+
+            val result = commandProcessor.onPlayerCommandText(player, commandLine)
+
+            assertThat(result)
+                    .isEqualTo(OnPlayerCommandTextListener.Result.Processed)
+            verify {
+                commandExecutor.execute(commandDefinition, arrayOf(player, 123))
+            }
+        }
+
+        @Test
+        fun givenParametersAreNotResolvedItShouldNotExecuteCommands() {
+            val commandLine = "/hi there"
+            val commandDefinition = CommandDefinition(
+                    name = "hi",
+                    groupName = "there",
+                    method = method,
+                    commandsInstance = TestCommands,
+                    parameters = listOf()
+            )
+            every { commandParser.parse(commandLine) } returns ParsedCommand("hi", listOf("there", "123"))
+            every { commandRegistry.getCommandDefinition("hi", "there") } returns commandDefinition
+            every {
+                commandAccessCheckExecutor.checkAccess(player, commandDefinition, listOf("123"))
+            } returns null
+            every {
+                commandParametersResolver.resolve(player, commandDefinition, listOf("123"))
+            } returns CommandParametersResolver.Result.Error(OnPlayerCommandTextListener.Result.Processed)
+            every { commandExecutor.execute(any(), any()) } returns OnPlayerCommandTextListener.Result.Processed
+
+            val result = commandProcessor.onPlayerCommandText(player, commandLine)
+
+            assertThat(result)
+                    .isEqualTo(OnPlayerCommandTextListener.Result.Processed)
+            verify {
+                commandExecutor wasNot Called
+            }
+        }
+
+        @Test
+        fun givenExceptionIsThrownItShouldExecutorCommandErrorHandler() {
+            every { player.name } returns "hans.wurst"
+            val commandLine = "/hi there"
+            val commandDefinition = CommandDefinition(
+                    name = "hi",
+                    groupName = "there",
+                    method = method,
+                    commandsInstance = TestCommands,
+                    parameters = listOf()
+            )
+            every { commandParser.parse(commandLine) } returns ParsedCommand("hi", listOf("there", "123"))
+            every { commandRegistry.getCommandDefinition("hi", "there") } returns commandDefinition
+            every {
+                commandAccessCheckExecutor.checkAccess(player, commandDefinition, listOf("123"))
+            } returns null
+            every {
+                commandParametersResolver.resolve(player, commandDefinition, listOf("123"))
+            } returns CommandParametersResolver.Result.ParameterValues(arrayOf(player, 123))
+            val exception = Exception("shit happened")
+            every { commandExecutor.execute(any(), any()) } throws exception
+            every {
+                defaultCommandErrorHandler.handle(any(), any(), any())
+            } returns OnPlayerCommandTextListener.Result.Processed
+
+            val result = commandProcessor.onPlayerCommandText(player, commandLine)
+
+            assertThat(result)
+                    .isEqualTo(OnPlayerCommandTextListener.Result.Processed)
+            verify { defaultCommandErrorHandler.handle(player, commandLine, exception) }
+        }
+
+    }
+
+    @Nested
+    inner class CommandWithoutGroupTests {
+
+        @Test
+        fun givenAccessIsDeniedItShouldReturn() {
+            val commandLine = "/hi there"
+            val commandDefinition = CommandDefinition(
+                    name = "hi",
+                    method = method,
+                    commandsInstance = TestCommands,
+                    parameters = listOf()
+            )
+            every { commandParser.parse(commandLine) } returns ParsedCommand("hi", listOf("there", "lol"))
+            every { commandRegistry.getCommandDefinition("hi", "there") } returns commandDefinition
+            every {
+                commandAccessCheckExecutor.checkAccess(player, commandDefinition, listOf("there", "lol"))
+            } returns OnPlayerCommandTextListener.Result.Processed
+
+            val result = commandProcessor.onPlayerCommandText(player, commandLine)
+
+            assertThat(result)
+                    .isEqualTo(OnPlayerCommandTextListener.Result.Processed)
+        }
+
+        @Test
+        fun givenParametersAreResolvedItShouldExecuteCommands() {
+            val commandLine = "/hi there"
+            val commandDefinition = CommandDefinition(
+                    name = "hi",
+                    method = method,
+                    commandsInstance = TestCommands,
+                    parameters = listOf()
+            )
+            val stringParameterValues = listOf("there", "123")
+            every { commandParser.parse(commandLine) } returns ParsedCommand("hi", stringParameterValues)
+            every { commandRegistry.getCommandDefinition("hi", "there") } returns commandDefinition
+            every {
+                commandAccessCheckExecutor.checkAccess(player, commandDefinition, stringParameterValues)
+            } returns null
+            val parameterValues = arrayOf(player, "there", 123)
+            every {
+                commandParametersResolver.resolve(player, commandDefinition, stringParameterValues)
+            } returns CommandParametersResolver.Result.ParameterValues(parameterValues)
+            every { commandExecutor.execute(any(), any()) } returns OnPlayerCommandTextListener.Result.Processed
+
+            val result = commandProcessor.onPlayerCommandText(player, commandLine)
+
+            assertThat(result)
+                    .isEqualTo(OnPlayerCommandTextListener.Result.Processed)
+            verify {
+                commandExecutor.execute(commandDefinition, parameterValues)
+            }
+        }
+
+        @Test
+        fun givenParametersAreNotResolvedItShouldNotExecuteCommands() {
+            val commandLine = "/hi there"
+            val commandDefinition = CommandDefinition(
+                    name = "hi",
+                    method = method,
+                    commandsInstance = TestCommands,
+                    parameters = listOf()
+            )
+            val stringParameterValues = listOf("there", "123")
+            every { commandParser.parse(commandLine) } returns ParsedCommand("hi", stringParameterValues)
+            every { commandRegistry.getCommandDefinition("hi", "there") } returns commandDefinition
+            every {
+                commandAccessCheckExecutor.checkAccess(player, commandDefinition, stringParameterValues)
+            } returns null
+            every {
+                commandParametersResolver.resolve(player, commandDefinition, stringParameterValues)
+            } returns CommandParametersResolver.Result.Error(OnPlayerCommandTextListener.Result.Processed)
+            every { commandExecutor.execute(any(), any()) } returns OnPlayerCommandTextListener.Result.Processed
+
+            val result = commandProcessor.onPlayerCommandText(player, commandLine)
+
+            assertThat(result)
+                    .isEqualTo(OnPlayerCommandTextListener.Result.Processed)
+            verify {
+                commandExecutor wasNot Called
+            }
+        }
+
+        @Test
+        fun givenExceptionIsThrownItShouldExecutorCommandErrorHandler() {
+            every { player.name } returns "hans.wurst"
+            val commandLine = "/hi there"
+            val commandDefinition = CommandDefinition(
+                    name = "hi",
+                    method = method,
+                    commandsInstance = TestCommands,
+                    parameters = listOf()
+            )
+            val stringParameterValues = listOf("there", "123")
+            every { commandParser.parse(commandLine) } returns ParsedCommand("hi", stringParameterValues)
+            every { commandRegistry.getCommandDefinition("hi", "there") } returns commandDefinition
+            every {
+                commandAccessCheckExecutor.checkAccess(player, commandDefinition, stringParameterValues)
+            } returns null
+            every {
+                commandParametersResolver.resolve(player, commandDefinition, listOf("there", "123"))
+            } returns CommandParametersResolver.Result.ParameterValues(arrayOf(player, "there", 123))
+            val exception = Exception("shit happened")
+            every { commandExecutor.execute(any(), any()) } throws exception
+            every {
+                defaultCommandErrorHandler.handle(any(), any(), any())
+            } returns OnPlayerCommandTextListener.Result.Processed
+
+            val result = commandProcessor.onPlayerCommandText(player, commandLine)
+
+            assertThat(result)
+                    .isEqualTo(OnPlayerCommandTextListener.Result.Processed)
+            verify { defaultCommandErrorHandler.handle(player, commandLine, exception) }
+        }
+
+    }
+
+    private object TestCommands : Commands()
+}
