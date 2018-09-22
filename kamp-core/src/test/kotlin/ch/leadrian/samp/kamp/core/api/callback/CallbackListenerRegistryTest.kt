fef5b6110f245ee2148f8f2fa9b191c8b7351dce
TheWishWithin <thewishwithin@gmail.com>
2018-09-22 21:20:53 +0200

    Removed unit test lazily abused for code generation

--- kamp-core/src/test/kotlin/ch/leadrian/samp/kamp/core/api/callback/CallbackListenerRegistryTest.kt
+++ kamp-core/src/test/kotlin/ch/leadrian/samp/kamp/core/api/callback/CallbackListenerRegistryTest.kt
@@ -1,13 +1,7 @@
 package ch.leadrian.samp.kamp.core.api.callback
 
-import com.google.common.reflect.ClassPath
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.util.*
-import kotlin.reflect.KClass
-import kotlin.reflect.full.functions
 
 internal class CallbackListenerRegistryTest {
 
@@ -81,61 +75,6 @@ internal class CallbackListenerRegistryTest {
 
         assertThat(callbackListenerRegistry.listeners.toList())
                 .containsExactlyInAnyOrder(FooCallbackListener)
-    }
-
-    @Test
-    fun generateClasses() {
-        ClassPath.from(this::class.java.classLoader).allClasses.filter {
-            it.packageName == "ch.leadrian.samp.kamp.core.api.callback"
-        }.map {
-            it.load()
-        }.filter {
-            it.simpleName.startsWith("On") && it.simpleName.endsWith("Listener")
-        }.forEach { clazz ->
-            val simpleName = clazz.simpleName
-            val className = "${simpleName}Processor"
-            Files.newBufferedWriter(Paths.get("src", "main", "kotlin", "ch", "leadrian", "samp", "kamp", "core", "runtime", "callback", "$className.kt")).use { writer ->
-                writer.apply {
-                    val callbackMethod = clazz.kotlin.functions.first { it.name.startsWith("on") }
-                    val packageImports = TreeSet<String>()
-                    packageImports += "ch.leadrian.samp.kamp.core.api.callback.CallbackListenerRegistry"
-                    packageImports += "ch.leadrian.samp.kamp.core.api.callback.$simpleName"
-                    packageImports += "javax.inject.Inject"
-                    packageImports += "javax.inject.Singleton"
-                    packageImports += callbackMethod.parameters.drop(1).map { it.type.classifier as KClass<*> }
-                            .filter { it.java.getPackage() != null }
-                            .filter { it.java.getPackage().name != "kotlin" && it.java.getPackage().name != "java.lang" }
-                            .map { it.java.name }
-
-                    write("package ch.leadrian.samp.kamp.core.runtime.callback\n\n")
-                    packageImports.forEach {
-                        write("import $it\n")
-                    }
-                    write("\n")
-                    write("@Singleton\n")
-                    write("internal class $className\n")
-                    write("@Inject\n")
-                    write("constructor() : CallbackListenerRegistry<$simpleName>($simpleName::class), $simpleName {\n\n")
-                    callbackMethod.apply {
-                        writer.write("    override fun $name(")
-                        writer.write(parameters.drop(1).joinToString(", ") {
-                            val type = (it.type.classifier as KClass<*>).simpleName
-                            "${it.name}: $type"
-                        })
-                        writer.write(")")
-                        if (returnType.toString() != "kotlin.Unit") {
-                            writer.write(": $returnType")
-                        }
-                        writer.write(" {\n")
-                        writer.write("        getListeners().forEach {\n")
-                        writer.write("            it.$name(${this.parameters.drop(1).map { it.name }.joinToString(", ")})\n")
-                        writer.write("        }\n")
-                        writer.write("    }\n\n")
-                    }
-                    write("}\n")
-                }
-            }
-        }
     }
 
     @Priority(5, CallbackListener::class)
