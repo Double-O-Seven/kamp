b87dfdef61598f5a3e0750863e351d9d3a2cd718
leadrian <thewishwithin@gmail.com>
2018-09-24 23:59:53 +0200

    Added CallbackListenerManager

---
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/callback/CallbackListenerManager.kt
@@ -0,0 +1,64 @@
+package ch.leadrian.samp.kamp.core.api.callback
+
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.reflect.KClass
+
+@Singleton
+class CallbackListenerManager
+@Inject
+internal constructor(callbackListenerRegistries: Set<@JvmSuppressWildcards CallbackListenerRegistry<*>>) {
+
+    private val callbackListenerRegistries: MutableMap<KClass<out Any>, CallbackListenerRegistry<*>> = mutableMapOf()
+
+    init {
+        callbackListenerRegistries.forEach {
+            this.callbackListenerRegistries.merge(it.listenerClass, it) { oldValue, newValue ->
+                throw IllegalArgumentException("Duplicate listeners for class ${it.listenerClass}: $oldValue, $newValue")
+            }
+        }
+    }
+
+    fun <T : Any> registerOnlyAs(clazz: KClass<T>, listener: T, priority: Int? = null) {
+        callbackListenerRegistries[clazz]?.register(listener, priority)
+                ?: throw IllegalStateException("$listener could not be registered as $clazz")
+    }
+
+    inline fun <reified T : Any> registerOnlyAs(listener: T, priority: Int? = null) {
+        registerOnlyAs(T::class, listener, priority)
+    }
+
+    fun <T : Any> unregisterOnlyAs(clazz: KClass<T>, listener: T) {
+        val unregistered = callbackListenerRegistries[clazz]?.unregister(listener) ?: false
+        if (!unregistered) {
+            throw IllegalStateException("$listener could not be unregistered as $clazz")
+        }
+    }
+
+    inline fun <reified T : Any> unregisterOnlyAs(listener: T) {
+        unregisterOnlyAs(T::class, listener)
+    }
+
+    fun register(listener: Any, priority: Int? = null) {
+        var registrations = 0
+        callbackListenerRegistries.forEach {
+            if (it.value.register(listener, priority)) {
+                registrations++
+            }
+        }
+        if (registrations == 0) {
+            throw IllegalStateException("$listener was not registered as any listener")
+        }
+    }
+
+    fun unregister(listener: Any) {
+        var unregistered = false
+        callbackListenerRegistries.forEach {
+            unregistered = it.value.unregister(listener) || unregistered
+        }
+        if (!unregistered) {
+            throw IllegalStateException("$listener was not unregistered as any listener")
+        }
+    }
+
+}
---
+++ kamp-core/src/test/kotlin/ch/leadrian/samp/kamp/core/api/callback/CallbackListenerManagerTest.kt
@@ -0,0 +1,242 @@
+package ch.leadrian.samp.kamp.core.api.callback
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class CallbackListenerManagerTest {
+
+    @Nested
+    inner class PostConstructionTests {
+
+        private val fooCallbackListenerRegistry = mockk<FooCallbackListenerRegistry>()
+        private val barCallbackListenerRegistry = mockk<BarCallbackListenerRegistry>()
+        private lateinit var callbackListenerManager: CallbackListenerManager
+
+        @BeforeEach
+        fun setUp() {
+            every { fooCallbackListenerRegistry.listenerClass } returns FooCallback::class
+            every { barCallbackListenerRegistry.listenerClass } returns BarCallback::class
+            callbackListenerManager = CallbackListenerManager(setOf(fooCallbackListenerRegistry, barCallbackListenerRegistry))
+        }
+
+        @Nested
+        inner class RegisterTests {
+
+            @Test
+            fun shouldRegisterListenerWithUnspecifiedInterfaces() {
+                every {
+                    fooCallbackListenerRegistry.register(any(), any())
+                } returns true
+                every {
+                    barCallbackListenerRegistry.register(any(), any())
+                } returns false
+
+                callbackListenerManager.register(FooService, 1337)
+
+                verify {
+                    fooCallbackListenerRegistry.register(FooService, 1337)
+                    barCallbackListenerRegistry.register(FooService, 1337)
+                }
+            }
+
+            @Test
+            fun shouldRegisterWithSpecifiedInterface() {
+                every {
+                    fooCallbackListenerRegistry.register(any(), any())
+                } returns true
+
+                callbackListenerManager.registerOnlyAs(FooCallback::class, FooService, 1337)
+
+                verify {
+                    fooCallbackListenerRegistry.register(FooService, 1337)
+                }
+                verify(exactly = 0) {
+                    barCallbackListenerRegistry.register(any(), any())
+                }
+            }
+
+            @Test
+            fun shouldInlineRegisterWithSpecifiedInterface() {
+                every {
+                    fooCallbackListenerRegistry.register(any(), any())
+                } returns true
+
+                callbackListenerManager.registerOnlyAs<FooCallback>(FooService, 1337)
+
+                verify {
+                    fooCallbackListenerRegistry.register(FooService, 1337)
+                }
+                verify(exactly = 0) {
+                    barCallbackListenerRegistry.register(any(), any())
+                }
+            }
+
+            @Test
+            fun givenNoRegistryForSpecifiedClassRegisterShouldThrowException() {
+                val caughtThrowable = catchThrowable {
+                    callbackListenerManager.registerOnlyAs(QuxService::class, QuxService, 1337)
+                }
+
+                assertThat(caughtThrowable)
+                        .isInstanceOf(IllegalStateException::class.java)
+                verify(exactly = 0) {
+                    fooCallbackListenerRegistry.register(any(), any())
+                    barCallbackListenerRegistry.register(any(), any())
+                }
+            }
+
+            @Test
+            fun givenNoRegistryForSpecifiedClassInlineRegisterShouldThrowException() {
+                val caughtThrowable = catchThrowable {
+                    callbackListenerManager.registerOnlyAs(QuxService, 1337)
+                }
+
+                assertThat(caughtThrowable)
+                        .isInstanceOf(IllegalStateException::class.java)
+                verify(exactly = 0) {
+                    fooCallbackListenerRegistry.register(any(), any())
+                    barCallbackListenerRegistry.register(any(), any())
+                }
+            }
+        }
+
+        @Nested
+        inner class UnregisterTests {
+
+            @Test
+            fun shouldUnregisterListenerWithUnspecifiedInterfaces() {
+                every {
+                    fooCallbackListenerRegistry.unregister(any())
+                } returns true
+                every {
+                    barCallbackListenerRegistry.unregister(any())
+                } returns false
+
+                callbackListenerManager.unregister(FooService)
+
+                verify {
+                    fooCallbackListenerRegistry.unregister(FooService)
+                    barCallbackListenerRegistry.unregister(FooService)
+                }
+            }
+
+            @Test
+            fun givenListenerWasNotUnregisteredFromAnyRegistryItShouldThrowException() {
+                every {
+                    fooCallbackListenerRegistry.unregister(any())
+                } returns false
+                every {
+                    barCallbackListenerRegistry.unregister(any())
+                } returns false
+
+                val caughtThrowable = catchThrowable { callbackListenerManager.unregister(FooService) }
+
+                assertThat(caughtThrowable)
+                        .isInstanceOf(IllegalStateException::class.java)
+                verify {
+                    fooCallbackListenerRegistry.unregister(FooService)
+                    barCallbackListenerRegistry.unregister(FooService)
+                }
+            }
+
+            @Test
+            fun shouldUnregisterWithSpecifiedInterface() {
+                every {
+                    fooCallbackListenerRegistry.unregister(any())
+                } returns true
+
+                callbackListenerManager.unregisterOnlyAs(FooCallback::class, FooService)
+
+                verify {
+                    fooCallbackListenerRegistry.unregister(FooService)
+                }
+                verify(exactly = 0) {
+                    barCallbackListenerRegistry.unregister(any())
+                }
+            }
+
+            @Test
+            fun shouldInlineUnregisterWithSpecifiedInterface() {
+                every {
+                    fooCallbackListenerRegistry.unregister(any())
+                } returns true
+
+                callbackListenerManager.unregisterOnlyAs<FooCallback>(FooService)
+
+                verify {
+                    fooCallbackListenerRegistry.unregister(FooService)
+                }
+                verify(exactly = 0) {
+                    barCallbackListenerRegistry.unregister(any())
+                }
+            }
+
+            @Test
+            fun givenUnregisterOnlyAsReturnsFalseItShouldThrowException() {
+                every {
+                    fooCallbackListenerRegistry.unregister(any())
+                } returns false
+
+                val caughtThrowable = catchThrowable {
+                    callbackListenerManager.unregisterOnlyAs(FooCallback::class, FooService)
+                }
+
+                assertThat(caughtThrowable)
+                        .isInstanceOf(IllegalStateException::class.java)
+            }
+
+            @Test
+            fun givenNoRegistryForSpecifiedClassUnregisterOnlyAsShouldThrowException() {
+                every {
+                    fooCallbackListenerRegistry.unregister(any())
+                } returns false
+
+                val caughtThrowable = catchThrowable {
+                    callbackListenerManager.unregisterOnlyAs(QuxService::class, QuxService)
+                }
+
+                assertThat(caughtThrowable)
+                        .isInstanceOf(IllegalStateException::class.java)
+            }
+
+            @Test
+            fun givenInlineUnregisterOnlyAsReturnsFalseItShouldThrowException() {
+                val caughtThrowable = catchThrowable {
+                    callbackListenerManager.unregisterOnlyAs(FooService)
+                }
+
+                assertThat(caughtThrowable)
+                        .isInstanceOf(IllegalStateException::class.java)
+            }
+
+            @Test
+            fun givenInlineNoRegistryForSpecifiedClassUnregisterOnlyAsShouldThrowException() {
+                val caughtThrowable = catchThrowable {
+                    callbackListenerManager.unregisterOnlyAs(QuxService)
+                }
+
+                assertThat(caughtThrowable)
+                        .isInstanceOf(IllegalStateException::class.java)
+            }
+        }
+    }
+
+    private interface FooCallback
+
+    private interface BarCallback
+
+    private class FooCallbackListenerRegistry : CallbackListenerRegistry<FooCallback>(FooCallback::class)
+
+    private class BarCallbackListenerRegistry : CallbackListenerRegistry<BarCallback>(BarCallback::class)
+
+    private object FooService : FooCallback
+
+    private object QuxService
+
+}
