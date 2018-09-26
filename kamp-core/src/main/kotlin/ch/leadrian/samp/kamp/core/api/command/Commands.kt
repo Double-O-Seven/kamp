c36ba8ed4d8c4a795ff2f4792974834664dd3d0e
leadrian <thewishwithin@gmail.com>
2018-09-26 21:21:54 +0200

    Extended Commands class API

--- kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/command/Commands.kt
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/command/Commands.kt
@@ -1,8 +1,13 @@
 package ch.leadrian.samp.kamp.core.api.command
 
 import ch.leadrian.samp.kamp.core.api.entity.Player
+import ch.leadrian.samp.kamp.core.runtime.callback.OnPlayerCommandTextHandler
+import javax.inject.Inject
 
 abstract class Commands {
+
+    @Inject
+    private lateinit var onPlayerCommandTextHandler: OnPlayerCommandTextHandler
 
     var groupName: String? = null
         internal set
@@ -10,8 +15,12 @@ abstract class Commands {
     lateinit var definitions: List<CommandDefinition>
         internal set
 
-    fun showCommandList(title: String, player: Player, executeOnSelect: Boolean = true) {
+    fun showCommandList(title: String, player: Player, executeOnSelect: Boolean = true, maxCommandsPerPage: Int = 30) {
         TODO()
+    }
+
+    protected fun executeCommand(player: Player, commandText: String) {
+        onPlayerCommandTextHandler.onPlayerCommandText(player, commandText)
     }
 
 }
