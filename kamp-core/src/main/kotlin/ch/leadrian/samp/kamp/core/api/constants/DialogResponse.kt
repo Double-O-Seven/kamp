cc2bb673aa036d13a61a99abe911fa8ca2d501df
leadrian <thewishwithin@gmail.com>
2018-09-30 21:52:29 +0200

    Added DialogResponse.isCancelButton

--- kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/constants/DialogResponse.kt
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/constants/DialogResponse.kt
@@ -1,8 +1,8 @@
 package ch.leadrian.samp.kamp.core.api.constants
 
-enum class DialogResponse(override val value: Int) : ConstantValue<Int> {
-    LEFT_BUTTON(1),
-    RIGHT_BUTTON(0);
+enum class DialogResponse(override val value: Int, val isCancelButton: Boolean) : ConstantValue<Int> {
+    LEFT_BUTTON(1, false),
+    RIGHT_BUTTON(0, true);
 
     companion object : ConstantValueRegistry<Int, DialogResponse>(*DialogResponse.values())
 }
