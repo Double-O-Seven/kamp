3d88550476d0b5061a3f5d31062d7f24674fbff2
leadrian <thewishwithin@gmail.com>
2018-10-01 23:28:37 +0200

    Implemented ServerService

--- kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/service/ServerService.kt
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/service/ServerService.kt
@@ -3,29 +3,74 @@ package ch.leadrian.samp.kamp.core.api.service
 import ch.leadrian.samp.kamp.core.api.data.Animation
 import ch.leadrian.samp.kamp.core.api.entity.ConsoleVars
 import ch.leadrian.samp.kamp.core.api.entity.ServerVars
+import ch.leadrian.samp.kamp.core.runtime.SAMPNativeFunctionExecutor
+import ch.leadrian.samp.kamp.core.runtime.types.ReferenceString
 import java.nio.file.Path
+import java.nio.file.Paths
+import javax.inject.Inject
 
-interface ServerService {
+class ServerService
+@Inject
+internal constructor(private val nativeFunctionExecutor: SAMPNativeFunctionExecutor) {
 
-    fun setGameModeText(text: String)
+    private val serverVars = ServerVars(nativeFunctionExecutor)
 
-    fun setTeamCount(teamCount: Int)
+    private val consoleVars = ConsoleVars(nativeFunctionExecutor)
 
-    fun getServerVars(): ServerVars
+    private val dataDirectory = Paths.get(".", "Kamp", "data")
 
-    fun getConsoleVars(): ConsoleVars
+    fun setGameModeText(text: String) {
+        nativeFunctionExecutor.setGameModeText(text)
+    }
 
-    fun triggerGameModeExit()
+    fun setTeamCount(teamCount: Int) {
+        nativeFunctionExecutor.setTeamCount(teamCount)
+    }
 
-    fun sendRconCommand(command: String)
+    fun getServerVars(): ServerVars = serverVars
 
-    fun getNetworkStatistics(resultLength: Int = 401): String
+    fun getConsoleVars(): ConsoleVars = consoleVars
 
-    fun blockIpAddress(ipAddress: String, timeInMs: Int)
+    fun triggerGameModeExit() {
+        nativeFunctionExecutor.gameModeExit()
+    }
 
-    fun unblockIpAddress(ipAddress: String)
+    fun sendRconCommand(command: String) {
+        nativeFunctionExecutor.sendRconCommand(command)
+    }
 
-    fun getAnimationName(animationIndex: Int): Animation
+    fun getNetworkStatistics(resultLength: Int = 401): String {
+        val networkStatistics = ReferenceString()
+        nativeFunctionExecutor.getNetworkStats(networkStatistics, resultLength)
+        return networkStatistics.value ?: ""
+    }
 
-    fun getDataDirectory(): Path
+    fun blockIpAddress(ipAddress: String, timeInMs: Int) {
+        nativeFunctionExecutor.blockIpAddress(ipAddress, timeInMs)
+    }
+
+    fun unblockIpAddress(ipAddress: String) {
+        nativeFunctionExecutor.unBlockIpAddress(ipAddress)
+    }
+
+    fun getAnimationName(animationIndex: Int, animationLibrarySize: Int = 32, animationNameSize: Int = 32): Animation {
+        val animationLibrary = ReferenceString()
+        val animationName = ReferenceString()
+        nativeFunctionExecutor.getAnimationName(
+                index = animationIndex,
+                animlib = animationLibrary,
+                animlib_size = animationLibrarySize,
+                animname = animationName,
+                animname_size = animationNameSize
+        )
+        val animationLibraryValue = animationLibrary.value
+        val animationNameValue = animationName.value
+        if (animationLibraryValue != null && animationNameValue != null) {
+            return Animation.valueOf(library = animationLibraryValue, name = animationNameValue)
+        } else {
+            throw IllegalArgumentException("Invalid animation index: $animationIndex")
+        }
+    }
+
+    fun getDataDirectory(): Path = dataDirectory
 }
---
+++ kamp-core/src/test/kotlin/ch/leadrian/samp/kamp/core/api/service/ServerServiceTest.kt
@@ -0,0 +1,199 @@
+package ch.leadrian.samp.kamp.core.api.service
+
+import ch.leadrian.samp.kamp.core.api.data.Animation
+import ch.leadrian.samp.kamp.core.runtime.SAMPNativeFunctionExecutor
+import ch.leadrian.samp.kamp.core.runtime.types.ReferenceString
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
+internal class ServerServiceTest {
+
+    private lateinit var serverService: ServerService
+
+    private val nativeFunctionExecutor = mockk<SAMPNativeFunctionExecutor>()
+
+    @BeforeEach
+    fun setUp() {
+        serverService = ServerService(nativeFunctionExecutor)
+    }
+
+    @Test
+    fun shouldSetGameModeText() {
+        every { nativeFunctionExecutor.setGameModeText(any()) } returns true
+
+        serverService.setGameModeText("LVDM")
+
+        verify { nativeFunctionExecutor.setGameModeText("LVDM") }
+    }
+
+    @Test
+    fun shouldSetTeamCount() {
+        every { nativeFunctionExecutor.setTeamCount(any()) } returns true
+
+        serverService.setTeamCount(69)
+
+        verify { nativeFunctionExecutor.setTeamCount(69) }
+    }
+
+    @Test
+    fun shouldGetServerVars() {
+        val serverVars = serverService.getServerVars()
+
+        assertThat(serverVars)
+                .isNotNull
+    }
+
+    @Test
+    fun shouldGetConsoleVars() {
+        val consoleVars = serverService.getConsoleVars()
+
+        assertThat(consoleVars)
+                .isNotNull
+    }
+
+    @Test
+    fun shouldTriggerGameModeExit() {
+        every { nativeFunctionExecutor.gameModeExit() } returns true
+
+        serverService.triggerGameModeExit()
+
+        verify { nativeFunctionExecutor.gameModeExit() }
+    }
+
+    @Test
+    fun shouldSendRconCommand() {
+        every { nativeFunctionExecutor.sendRconCommand(any()) } returns true
+
+        serverService.sendRconCommand("ban 69")
+
+        verify { nativeFunctionExecutor.sendRconCommand("ban 69") }
+    }
+
+    @Nested
+    inner class GetNetworkStatisticsTests {
+
+        @Test
+        fun shouldReturnNetworkStatistics() {
+            every { nativeFunctionExecutor.getNetworkStats(any(), 32) } answers {
+                firstArg<ReferenceString>().value = "Ping: 69"
+                true
+            }
+
+            val networkStatistics = serverService.getNetworkStatistics(resultLength = 32)
+
+            assertThat(networkStatistics)
+                    .isEqualTo("Ping: 69")
+        }
+
+        @Test
+        fun givenReferenceStringValueIsNotSetItShouldReturnEmptyString() {
+            every { nativeFunctionExecutor.getNetworkStats(any(), any()) } returns true
+
+            val networkStatistics = serverService.getNetworkStatistics(resultLength = 32)
+
+            assertThat(networkStatistics)
+                    .isEmpty()
+        }
+
+    }
+
+    @Test
+    fun shouldBlockIpAddress() {
+        every { nativeFunctionExecutor.blockIpAddress(any(), any()) } returns true
+
+        serverService.blockIpAddress("127.0.0.1", 60000)
+
+        verify { nativeFunctionExecutor.blockIpAddress("127.0.0.1", 60000) }
+    }
+
+    @Test
+    fun shouldUnblockIpAddress() {
+        every { nativeFunctionExecutor.unBlockIpAddress(any()) } returns true
+
+        serverService.unblockIpAddress("127.0.0.1")
+
+        verify { nativeFunctionExecutor.unBlockIpAddress("127.0.0.1") }
+    }
+
+    @Nested
+    inner class GetAnimationNameTests {
+
+        @Test
+        fun shouldReturnAnimation() {
+            every {
+                nativeFunctionExecutor.getAnimationName(1337, any(), 32, any(), 32)
+            } answers {
+                secondArg<ReferenceString>().value = "TEST_LIB"
+                arg<ReferenceString>(3).value = "test_anim"
+                true
+            }
+
+            val animation = serverService.getAnimationName(1337)
+
+            assertThat(animation)
+                    .isEqualTo(Animation.valueOf(library = "TEST_LIB", name = "test_anim"))
+        }
+
+        @Test
+        fun givenNoReferenceStringValueIsSetItShouldThrowException() {
+            every {
+                nativeFunctionExecutor.getAnimationName(any(), any(), any(), any(), any())
+            } returns true
+
+            val caughtThrowable = catchThrowable { serverService.getAnimationName(1337) }
+
+            assertThat(caughtThrowable)
+                    .isInstanceOf(IllegalArgumentException::class.java)
+                    .hasMessage("Invalid animation index: 1337")
+        }
+
+        @Test
+        fun givenLibraryNameIsNotSetItShouldThrowException() {
+            every {
+                nativeFunctionExecutor.getAnimationName(1337, any(), 32, any(), 32)
+            } answers {
+                arg<ReferenceString>(3).value = "test_anim"
+                true
+            }
+
+            val caughtThrowable = catchThrowable { serverService.getAnimationName(1337) }
+
+            assertThat(caughtThrowable)
+                    .isInstanceOf(IllegalArgumentException::class.java)
+                    .hasMessage("Invalid animation index: 1337")
+        }
+
+        @Test
+        fun givenAnimationNameIsNotSetItShouldThrowException() {
+            every {
+                nativeFunctionExecutor.getAnimationName(1337, any(), 32, any(), 32)
+            } answers {
+                secondArg<ReferenceString>().value = "TEST_LIB"
+                true
+            }
+
+            val caughtThrowable = catchThrowable { serverService.getAnimationName(1337) }
+
+            assertThat(caughtThrowable)
+                    .isInstanceOf(IllegalArgumentException::class.java)
+                    .hasMessage("Invalid animation index: 1337")
+        }
+
+    }
+
+    @Test
+    fun shouldReturnDataDirectoryPath() {
+        val dataDirectory = serverService.getDataDirectory()
+
+        assertThat(dataDirectory)
+                .isEqualTo(Paths.get(".", "Kamp", "data"))
+    }
+
+}
