365d211466bb733c5c3e5b4dc1542191b08e7802
leadrian <thewishwithin@gmail.com>
2018-10-02 20:12:03 +0200

    Implemented VehicleService

--- kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/service/VehicleService.kt
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/service/VehicleService.kt
@@ -1,57 +1,114 @@
 package ch.leadrian.samp.kamp.core.api.service
 
-import ch.leadrian.samp.kamp.core.api.constants.VehicleColor
 import ch.leadrian.samp.kamp.core.api.constants.VehicleModel
 import ch.leadrian.samp.kamp.core.api.constants.VehicleModelInfoType
 import ch.leadrian.samp.kamp.core.api.data.AngledLocation
 import ch.leadrian.samp.kamp.core.api.data.Position
 import ch.leadrian.samp.kamp.core.api.data.Vector3D
+import ch.leadrian.samp.kamp.core.api.data.VehicleColors
+import ch.leadrian.samp.kamp.core.api.data.vector3DOf
 import ch.leadrian.samp.kamp.core.api.entity.Vehicle
 import ch.leadrian.samp.kamp.core.api.entity.id.VehicleId
+import ch.leadrian.samp.kamp.core.api.exception.NoSuchEntityException
+import ch.leadrian.samp.kamp.core.runtime.SAMPNativeFunctionExecutor
+import ch.leadrian.samp.kamp.core.runtime.entity.factory.VehicleFactory
+import ch.leadrian.samp.kamp.core.runtime.entity.registry.VehicleRegistry
+import ch.leadrian.samp.kamp.core.runtime.types.ReferenceFloat
+import javax.inject.Inject
+import javax.inject.Singleton
 
-interface VehicleService {
+@Singleton
+class VehicleService
+@Inject
+internal constructor(
+        private val vehicleFactory: VehicleFactory,
+        private val vehicleRegistry: VehicleRegistry,
+        private val nativeFunctionExecutor: SAMPNativeFunctionExecutor
+) {
+
+    private val modelInfoCache = mutableMapOf<Pair<VehicleModel, VehicleModelInfoType>, Vector3D>()
 
     fun createVehicle(
             model: VehicleModel,
+            colors: VehicleColors,
             position: Position,
-            color1: VehicleColor,
-            color: VehicleColor,
-            respawnDelay: Int,
-            addSiren: Boolean = false
-    ): Vehicle
+            addSiren: Boolean = false,
+            respawnDelay: Int
+    ): Vehicle = vehicleFactory.create(
+            model = model,
+            coordinates = position,
+            rotation = position.angle,
+            addSiren = addSiren,
+            respawnDelay = respawnDelay,
+            colors = colors
+    )
 
     fun createVehicle(
             model: VehicleModel,
+            colors: VehicleColors,
             coordinates: Vector3D,
             rotation: Float,
-            color1: VehicleColor,
-            color: VehicleColor,
-            respawnDelay: Int,
-            addSiren: Boolean = false
-    ): Vehicle
+            addSiren: Boolean = false,
+            respawnDelay: Int
+    ): Vehicle = vehicleFactory.create(
+            model = model,
+            coordinates = coordinates,
+            rotation = rotation,
+            addSiren = addSiren,
+            respawnDelay = respawnDelay,
+            colors = colors
+    )
 
     fun createVehicle(
             model: VehicleModel,
+            colors: VehicleColors,
             angledLocation: AngledLocation,
-            color1: VehicleColor,
-            color: VehicleColor,
-            respawnDelay: Int,
-            addSiren: Boolean = false
-    ): Vehicle
+            addSiren: Boolean = false,
+            respawnDelay: Int
+    ): Vehicle = vehicleFactory.create(
+            model = model,
+            coordinates = angledLocation,
+            rotation = angledLocation.angle,
+            addSiren = addSiren,
+            respawnDelay = respawnDelay,
+            colors = colors
+    ).apply {
+        virtualWorldId = angledLocation.virtualWorldId
+        interiorId = angledLocation.interiorId
+    }
 
-    fun useManualVehicleLightsAndEngine()
+    fun useManualVehicleLightsAndEngine() {
+        nativeFunctionExecutor.manualVehicleEngineAndLights()
+    }
 
-    fun isValid(vehicleId: VehicleId): Boolean
+    fun isValidVehicle(vehicleId: VehicleId): Boolean = vehicleRegistry[vehicleId] != null
 
-    fun getVehicle(vehicleId: VehicleId): Vehicle
+    fun getVehicle(vehicleId: VehicleId): Vehicle =
+            vehicleRegistry[vehicleId] ?: throw NoSuchEntityException("No vehicle with ID ${vehicleId.value}")
 
-    fun getAllVehicles(): List<Vehicle>
+    fun getAllVehicles(): List<Vehicle> = vehicleRegistry.getAll()
 
-    fun getPoolSize(): Int
+    fun getPoolSize(): Int = nativeFunctionExecutor.getVehiclePoolSize()
 
-    fun enableTirePopping(enable: Boolean)
+    fun enableTirePopping() {
+        nativeFunctionExecutor.enableTirePopping(true)
+    }
 
-    fun enableFriendlyFire()
+    fun disableTirePopping() {
+        nativeFunctionExecutor.enableTirePopping(false)
+    }
 
-    fun getModelInfo(vehicleModel: VehicleModel, type: VehicleModelInfoType): Vector3D
+    fun enableFriendlyFire() {
+        nativeFunctionExecutor.enableVehicleFriendlyFire()
+    }
+
+    fun getModelInfo(vehicleModel: VehicleModel, type: VehicleModelInfoType): Vector3D {
+        return modelInfoCache.computeIfAbsent(vehicleModel to type) {
+            val x = ReferenceFloat()
+            val y = ReferenceFloat()
+            val z = ReferenceFloat()
+            nativeFunctionExecutor.getVehicleModelInfo(model = vehicleModel.value, infotype = type.value, X = x, Y = y, Z = z)
+            vector3DOf(x = x.value, y = y.value, z = z.value)
+        }
+    }
 }
---
+++ kamp-core/src/test/kotlin/ch/leadrian/samp/kamp/core/api/service/VehicleServiceTest.kt
@@ -0,0 +1,346 @@
+package ch.leadrian.samp.kamp.core.api.service
+
+import ch.leadrian.samp.kamp.core.api.constants.VehicleColor
+import ch.leadrian.samp.kamp.core.api.constants.VehicleModel
+import ch.leadrian.samp.kamp.core.api.constants.VehicleModelInfoType
+import ch.leadrian.samp.kamp.core.api.data.angledLocationOf
+import ch.leadrian.samp.kamp.core.api.data.positionOf
+import ch.leadrian.samp.kamp.core.api.data.vector3DOf
+import ch.leadrian.samp.kamp.core.api.data.vehicleColorsOf
+import ch.leadrian.samp.kamp.core.api.entity.Vehicle
+import ch.leadrian.samp.kamp.core.api.entity.id.VehicleId
+import ch.leadrian.samp.kamp.core.api.exception.NoSuchEntityException
+import ch.leadrian.samp.kamp.core.runtime.SAMPNativeFunctionExecutor
+import ch.leadrian.samp.kamp.core.runtime.entity.factory.VehicleFactory
+import ch.leadrian.samp.kamp.core.runtime.entity.registry.VehicleRegistry
+import ch.leadrian.samp.kamp.core.runtime.types.ReferenceFloat
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class VehicleServiceTest {
+
+    private lateinit var vehicleService: VehicleService
+
+    private val vehicleFactory = mockk<VehicleFactory>()
+    private val vehicleRegistry = mockk<VehicleRegistry>()
+    private val nativeFunctionExecutor = mockk<SAMPNativeFunctionExecutor>()
+
+    @BeforeEach
+    fun setUp() {
+        vehicleService = VehicleService(vehicleFactory, vehicleRegistry, nativeFunctionExecutor)
+    }
+
+    @Nested
+    inner class CreateVehicleTests {
+
+        private val vehicle = mockk<Vehicle>()
+
+        @BeforeEach
+        fun setUp() {
+            every { vehicleFactory.create(any(), any(), any(), any(), any(), any()) } returns vehicle
+        }
+
+        @Test
+        fun shouldCreateVehicleWithPosition() {
+            val createdVehicle = vehicleService.createVehicle(
+                    VehicleModel.FBIRANCHER,
+                    vehicleColorsOf(VehicleColor[3], VehicleColor[6]),
+                    positionOf(1f, 2f, 3f, 4f),
+                    true,
+                    60
+            )
+
+            assertThat(createdVehicle)
+                    .isEqualTo(vehicle)
+            verify {
+                vehicleFactory.create(
+                        VehicleModel.FBIRANCHER,
+                        vehicleColorsOf(VehicleColor[3], VehicleColor[6]),
+                        positionOf(1f, 2f, 3f, 4f),
+                        4f,
+                        true,
+                        60
+                )
+            }
+        }
+
+        @Test
+        fun shouldCreateVehicleWithCoordinatesAndAngle() {
+            val createdVehicle = vehicleService.createVehicle(
+                    VehicleModel.FBIRANCHER,
+                    vehicleColorsOf(VehicleColor[3], VehicleColor[6]),
+                    vector3DOf(1f, 2f, 3f),
+                    4f,
+                    true,
+                    60
+            )
+
+            assertThat(createdVehicle)
+                    .isEqualTo(vehicle)
+            verify {
+                vehicleFactory.create(
+                        VehicleModel.FBIRANCHER,
+                        vehicleColorsOf(VehicleColor[3], VehicleColor[6]),
+                        vector3DOf(1f, 2f, 3f),
+                        4f,
+                        true,
+                        60
+                )
+            }
+        }
+
+        @Test
+        fun shouldCreateVehicleWithAngledLocation() {
+            every { vehicle.virtualWorldId = any() } just Runs
+            every { vehicle.interiorId = any() } just Runs
+
+            val createdVehicle = vehicleService.createVehicle(
+                    VehicleModel.FBIRANCHER,
+                    vehicleColorsOf(VehicleColor[3], VehicleColor[6]),
+                    angledLocationOf(x = 1f, y = 2f, z = 3f, interiorId = 187, worldId = 69, angle = 4f),
+                    true,
+                    60
+            )
+
+            assertThat(createdVehicle)
+                    .isEqualTo(vehicle)
+            verify {
+                vehicleFactory.create(
+                        VehicleModel.FBIRANCHER,
+                        vehicleColorsOf(VehicleColor[3], VehicleColor[6]),
+                        angledLocationOf(x = 1f, y = 2f, z = 3f, interiorId = 187, worldId = 69, angle = 4f),
+                        4f,
+                        true,
+                        60
+                )
+                vehicle.interiorId = 187
+                vehicle.virtualWorldId = 69
+            }
+        }
+
+    }
+
+    @Test
+    fun shouldUseManualVehicleLightsAndEngine() {
+        every { nativeFunctionExecutor.manualVehicleEngineAndLights() } returns true
+
+        vehicleService.useManualVehicleLightsAndEngine()
+
+        verify { nativeFunctionExecutor.manualVehicleEngineAndLights() }
+    }
+
+    @Nested
+    inner class IsValidTests {
+
+        @Test
+        fun givenNoVehicleForVehicleIdItShouldReturnFalse() {
+            val vehicleId = VehicleId.valueOf(69)
+            every { vehicleRegistry[vehicleId] } returns null
+
+            val isValid = vehicleService.isValidVehicle(vehicleId)
+
+            assertThat(isValid)
+                    .isFalse()
+        }
+
+        @Test
+        fun givenVehicleForVehicleIdExistsItShouldReturnTrue() {
+            val vehicleId = VehicleId.valueOf(69)
+            val vehicle = mockk<Vehicle>()
+            every { vehicleRegistry[vehicleId] } returns vehicle
+
+            val isValid = vehicleService.isValidVehicle(vehicleId)
+
+            assertThat(isValid)
+                    .isTrue()
+        }
+    }
+
+    @Nested
+    inner class GetVehicleTests {
+
+        @Test
+        fun givenVehicleIdIsValidItShouldReturnVehicle() {
+            val vehicleId = VehicleId.valueOf(1337)
+            val expectedVehicle = mockk<Vehicle>()
+            every { vehicleRegistry[vehicleId] } returns expectedVehicle
+
+            val vehicle = vehicleService.getVehicle(vehicleId)
+
+            assertThat(vehicle)
+                    .isEqualTo(expectedVehicle)
+        }
+
+        @Test
+        fun givenInvalidVehicleIdItShouldThrowException() {
+            val vehicleId = VehicleId.valueOf(1337)
+            every { vehicleRegistry[vehicleId] } returns null
+
+            val caughtThrowable = catchThrowable {
+                vehicleService.getVehicle(vehicleId)
+            }
+
+            assertThat(caughtThrowable)
+                    .isInstanceOf(NoSuchEntityException::class.java)
+                    .hasMessage("No vehicle with ID 1337")
+        }
+
+    }
+
+    @Test
+    fun shouldReturnAllVehicles() {
+        val vehicle1 = mockk<Vehicle>()
+        val vehicle2 = mockk<Vehicle>()
+        every { vehicleRegistry.getAll() } returns listOf(vehicle1, vehicle2)
+
+        val vehicles = vehicleService.getAllVehicles()
+
+        assertThat(vehicles)
+                .containsExactly(vehicle1, vehicle2)
+    }
+
+    @Test
+    fun shouldReturnPoolSize() {
+        every { nativeFunctionExecutor.getVehiclePoolSize() } returns 500
+
+        val poolSize = vehicleService.getPoolSize()
+
+        assertThat(poolSize)
+                .isEqualTo(500)
+    }
+
+    @Test
+    fun shouldEnableTirePopping() {
+        every { nativeFunctionExecutor.enableTirePopping(any()) } returns true
+
+        vehicleService.enableTirePopping()
+
+        verify { nativeFunctionExecutor.enableTirePopping(true) }
+    }
+
+    @Test
+    fun shouldDisableTirePopping() {
+        every { nativeFunctionExecutor.enableTirePopping(any()) } returns true
+
+        vehicleService.disableTirePopping()
+
+        verify { nativeFunctionExecutor.enableTirePopping(false) }
+    }
+
+    @Test
+    fun shouldEnableFriendlyFire() {
+        every { nativeFunctionExecutor.enableVehicleFriendlyFire() } returns true
+
+        vehicleService.enableFriendlyFire()
+
+        verify { nativeFunctionExecutor.enableVehicleFriendlyFire() }
+    }
+
+    @Nested
+    inner class GetModelInfoTests {
+
+        @Test
+        fun shouldReturnModelInfo() {
+            every {
+                nativeFunctionExecutor.getVehicleModelInfo(VehicleModel.FBIRANCHER.value, VehicleModelInfoType.PETROLCAP.value, any(), any(), any())
+            } answers {
+                thirdArg<ReferenceFloat>().value = 1f
+                arg<ReferenceFloat>(3).value = 2f
+                arg<ReferenceFloat>(4).value = 3f
+                true
+            }
+
+            val modelInfo = vehicleService.getModelInfo(VehicleModel.FBIRANCHER, VehicleModelInfoType.PETROLCAP)
+
+            assertThat(modelInfo)
+                    .isEqualTo(vector3DOf(x = 1f, y = 2f, z = 3f))
+        }
+
+        @Test
+        fun givenSameModelAndDifferentTypeItShouldReturnDifferentResults() {
+            every {
+                nativeFunctionExecutor.getVehicleModelInfo(VehicleModel.FBIRANCHER.value, VehicleModelInfoType.PETROLCAP.value, any(), any(), any())
+            } answers {
+                thirdArg<ReferenceFloat>().value = 1f
+                arg<ReferenceFloat>(3).value = 2f
+                arg<ReferenceFloat>(4).value = 3f
+                true
+            }
+            every {
+                nativeFunctionExecutor.getVehicleModelInfo(VehicleModel.FBIRANCHER.value, VehicleModelInfoType.FRONTSEAT.value, any(), any(), any())
+            } answers {
+                thirdArg<ReferenceFloat>().value = 4f
+                arg<ReferenceFloat>(3).value = 5f
+                arg<ReferenceFloat>(4).value = 6f
+                true
+            }
+            vehicleService.getModelInfo(VehicleModel.FBIRANCHER, VehicleModelInfoType.PETROLCAP)
+
+            val modelInfo = vehicleService.getModelInfo(VehicleModel.FBIRANCHER, VehicleModelInfoType.FRONTSEAT)
+
+            assertThat(modelInfo)
+                    .isEqualTo(vector3DOf(x = 4f, y = 5f, z = 6f))
+        }
+
+        @Test
+        fun givenDifferentModelAndSameTypeItShouldReturnDifferentResults() {
+            every {
+                nativeFunctionExecutor.getVehicleModelInfo(VehicleModel.FBIRANCHER.value, VehicleModelInfoType.PETROLCAP.value, any(), any(), any())
+            } answers {
+                thirdArg<ReferenceFloat>().value = 1f
+                arg<ReferenceFloat>(3).value = 2f
+                arg<ReferenceFloat>(4).value = 3f
+                true
+            }
+            every {
+                nativeFunctionExecutor.getVehicleModelInfo(VehicleModel.BULLET.value, VehicleModelInfoType.PETROLCAP.value, any(), any(), any())
+            } answers {
+                thirdArg<ReferenceFloat>().value = 4f
+                arg<ReferenceFloat>(3).value = 5f
+                arg<ReferenceFloat>(4).value = 6f
+                true
+            }
+            vehicleService.getModelInfo(VehicleModel.FBIRANCHER, VehicleModelInfoType.PETROLCAP)
+
+            val modelInfo = vehicleService.getModelInfo(VehicleModel.BULLET, VehicleModelInfoType.PETROLCAP)
+
+            assertThat(modelInfo)
+                    .isEqualTo(vector3DOf(x = 4f, y = 5f, z = 6f))
+        }
+
+        @Test
+        fun givenDifferentModelAndDifferentTypeItShouldReturnDifferentResults() {
+            every {
+                nativeFunctionExecutor.getVehicleModelInfo(VehicleModel.FBIRANCHER.value, VehicleModelInfoType.PETROLCAP.value, any(), any(), any())
+            } answers {
+                thirdArg<ReferenceFloat>().value = 1f
+                arg<ReferenceFloat>(3).value = 2f
+                arg<ReferenceFloat>(4).value = 3f
+                true
+            }
+            every {
+                nativeFunctionExecutor.getVehicleModelInfo(VehicleModel.BULLET.value, VehicleModelInfoType.FRONTSEAT.value, any(), any(), any())
+            } answers {
+                thirdArg<ReferenceFloat>().value = 4f
+                arg<ReferenceFloat>(3).value = 5f
+                arg<ReferenceFloat>(4).value = 6f
+                true
+            }
+            vehicleService.getModelInfo(VehicleModel.FBIRANCHER, VehicleModelInfoType.PETROLCAP)
+
+            val modelInfo = vehicleService.getModelInfo(VehicleModel.BULLET, VehicleModelInfoType.FRONTSEAT)
+
+            assertThat(modelInfo)
+                    .isEqualTo(vector3DOf(x = 4f, y = 5f, z = 6f))
+        }
+    }
+
+}
