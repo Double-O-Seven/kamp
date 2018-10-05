e8d47e0a17877f485ce3024d3b3298171a847934
leadrian <thewishwithin@gmail.com>
2018-10-05 20:08:15 +0200

    Removed unnecessary TextProvider from DefaultUnknownCommandHandler

--- kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/command/DefaultUnknownCommandHandler.kt
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/command/DefaultUnknownCommandHandler.kt
@@ -5,14 +5,10 @@ import ch.leadrian.samp.kamp.core.api.callback.OnPlayerCommandTextListener
 import ch.leadrian.samp.kamp.core.api.data.Colors
 import ch.leadrian.samp.kamp.core.api.entity.Player
 import ch.leadrian.samp.kamp.core.api.text.MessageSender
-import ch.leadrian.samp.kamp.core.api.text.TextProvider
 import javax.inject.Inject
 
 open class DefaultUnknownCommandHandler
-@Inject constructor(
-        private val textProvider: TextProvider,
-        private val messageSender: MessageSender
-) : UnknownCommandHandler {
+@Inject constructor(private val messageSender: MessageSender) : UnknownCommandHandler {
 
     override fun handle(player: Player, command: String, parameters: List<String>): OnPlayerCommandTextListener.Result {
         messageSender.sendMessageToPlayer(player, Colors.RED, TextKeys.command.unknown, command, parameters.joinToString(" "))
