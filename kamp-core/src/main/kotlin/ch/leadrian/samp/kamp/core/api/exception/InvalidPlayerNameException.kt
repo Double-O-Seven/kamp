88582fd7913f9a16a92732c32d790335151e0a87
leadrian <thewishwithin@gmail.com>
2018-11-12 18:44:19 +0100

    Changed message of InvalidPlayerNameException

--- kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/exception/InvalidPlayerNameException.kt
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/exception/InvalidPlayerNameException.kt
@@ -1,3 +1,3 @@
 package ch.leadrian.samp.kamp.core.api.exception
 
-class InvalidPlayerNameException(val name: String, message: String) : RuntimeException(message)
+class InvalidPlayerNameException(val name: String, message: String) : RuntimeException("Invalid player name '$name': $message")
