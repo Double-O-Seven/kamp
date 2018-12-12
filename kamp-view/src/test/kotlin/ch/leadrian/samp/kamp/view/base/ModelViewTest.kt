fd10c4564ba8b91227f41022f6d4a0e79a67cc7a
leadrian <thewishwithin@gmail.com>
2018-12-13 00:05:23 +0100

    Added more tests to ModelView

--- kamp-view/src/test/kotlin/ch/leadrian/samp/kamp/view/base/ModelViewTest.kt
+++ kamp-view/src/test/kotlin/ch/leadrian/samp/kamp/view/base/ModelViewTest.kt
@@ -1,6 +1,8 @@
 package ch.leadrian.samp.kamp.view.base
 
 import ch.leadrian.samp.kamp.core.api.data.Colors
+import ch.leadrian.samp.kamp.core.api.data.vector3DOf
+import ch.leadrian.samp.kamp.core.api.data.vehicleColorsOf
 import ch.leadrian.samp.kamp.core.api.entity.Player
 import ch.leadrian.samp.kamp.core.api.service.PlayerTextDrawService
 import ch.leadrian.samp.kamp.view.ViewContext
@@ -68,6 +70,85 @@ internal class ModelViewTest {
 
             assertThat(modelView.modelId)
                     .isEqualTo(1337)
+        }
+
+    }
+
+    @Nested
+    inner class ZoomTests {
+
+        @Test
+        fun shouldBeInitializedWithOne() {
+            val zoom = modelView.zoom
+
+            assertThat(zoom)
+                    .isOne()
+        }
+
+        @Test
+        fun shouldSetZoom() {
+            modelView.zoom = 0.5f
+
+            assertThat(modelView.zoom)
+                    .isEqualTo(0.5f)
+        }
+
+        @Test
+        fun shouldSupplyZoom() {
+            modelView.zoom { 0.5f }
+
+            assertThat(modelView.zoom)
+                    .isEqualTo(0.5f)
+        }
+
+    }
+
+    @Nested
+    inner class RotationTests {
+
+        @Test
+        fun shouldSetRotation() {
+            modelView.rotation = vector3DOf(12.3f, 4.56f, 0.789f)
+
+            assertThat(modelView.rotation)
+                    .isEqualTo(vector3DOf(12.3f, 4.56f, 0.789f))
+        }
+
+        @Test
+        fun shouldSupplyRotation() {
+            modelView.rotation { vector3DOf(12.3f, 4.56f, 0.789f) }
+
+            assertThat(modelView.rotation)
+                    .isEqualTo(vector3DOf(12.3f, 4.56f, 0.789f))
+        }
+
+    }
+
+    @Nested
+    inner class VehicleColorsTests {
+
+        @Test
+        fun shouldInitiallyBeNull() {
+            val vehicleColors = modelView.vehicleColors
+
+            assertThat(vehicleColors)
+                    .isNull()
+        }
+
+        @Test
+        fun shouldSetVehicleColors() {
+            modelView.vehicleColors = vehicleColorsOf(3, 6)
+
+            assertThat(modelView.vehicleColors)
+                    .isEqualTo(vehicleColorsOf(3, 6))
+        }
+
+        @Test
+        fun shouldSupplyVehicleColors() {
+            modelView.vehicleColors { vehicleColorsOf(3, 6) }
+
+            assertThat(modelView.vehicleColors)
+                    .isEqualTo(vehicleColorsOf(3, 6))
         }
 
     }
