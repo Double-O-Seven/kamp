232f0e62f100f4231eb9ea055b417ee6d2b72cc6
leadrian <thewishwithin@gmail.com>
2019-01-29 00:33:59 +0100

    Added @NotNull annotations to StringEncoding

--- kamp-core/src/main/java/ch/leadrian/samp/kamp/core/runtime/StringEncoding.java
+++ kamp-core/src/main/java/ch/leadrian/samp/kamp/core/runtime/StringEncoding.java
@@ -1,19 +1,25 @@
 package ch.leadrian.samp.kamp.core.runtime;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.nio.charset.Charset;
+import java.util.Objects;
 
 public final class StringEncoding {
 
+    @NotNull
     private static Charset charset = Charset.forName("Cp1252");
 
     private StringEncoding() {
     }
 
+    @NotNull
     public static Charset getCharset() {
         return charset;
     }
 
-    public static void setCharset(Charset charset) {
+    public static void setCharset(@NotNull Charset charset) {
+        Objects.requireNonNull(charset);
         StringEncoding.charset = charset;
     }
 }
