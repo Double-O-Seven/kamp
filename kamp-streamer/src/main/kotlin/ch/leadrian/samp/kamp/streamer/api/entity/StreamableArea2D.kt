6e26914edfe7fcd24e0e447a653f8fe3740a506d
leadrian <thewishwithin@gmail.com>
2019-02-06 00:09:39 +0100

    Added comments regarding streamable area priorities

--- kamp-streamer/src/main/kotlin/ch/leadrian/samp/kamp/streamer/api/entity/StreamableArea2D.kt
+++ kamp-streamer/src/main/kotlin/ch/leadrian/samp/kamp/streamer/api/entity/StreamableArea2D.kt
@@ -2,4 +2,16 @@ package ch.leadrian.samp.kamp.streamer.api.entity
 
 import ch.leadrian.samp.kamp.core.api.data.Shape2D
 
-interface StreamableArea2D : StreamableArea, Shape2D
+interface StreamableArea2D : StreamableArea, Shape2D {
+
+    /**
+     * Defines the order in which [ch.leadrian.samp.kamp.streamer.api.callback.OnPlayerEnterStreamableAreaListener]s and
+     * [ch.leadrian.samp.kamp.streamer.api.callback.OnPlayerLeaveStreamableAreaListener]s are called.
+     * However, the priorities are only applied with respect to other [StreamableArea2D]s.
+     * There is no defined order between [StreamableArea3D]s and [StreamableArea2D]s.
+     *
+     * @see [Streamable.priority]
+     */
+    override val priority: Int
+
+}
--- kamp-streamer/src/main/kotlin/ch/leadrian/samp/kamp/streamer/api/entity/StreamableArea3D.kt
+++ kamp-streamer/src/main/kotlin/ch/leadrian/samp/kamp/streamer/api/entity/StreamableArea3D.kt
@@ -2,4 +2,16 @@ package ch.leadrian.samp.kamp.streamer.api.entity
 
 import ch.leadrian.samp.kamp.core.api.data.Shape3D
 
-interface StreamableArea3D : StreamableArea, Shape3D
+interface StreamableArea3D : StreamableArea, Shape3D {
+
+    /**
+     * Defines the order in which [ch.leadrian.samp.kamp.streamer.api.callback.OnPlayerEnterStreamableAreaListener]s and
+     * [ch.leadrian.samp.kamp.streamer.api.callback.OnPlayerLeaveStreamableAreaListener]s are called.
+     * However, the priorities are only applied with respect to other [StreamableArea3D]s.
+     * There is no defined order between [StreamableArea3D]s and [StreamableArea2D]s.
+     *
+     * @see [Streamable.priority]
+     */
+    override val priority: Int
+
+}
