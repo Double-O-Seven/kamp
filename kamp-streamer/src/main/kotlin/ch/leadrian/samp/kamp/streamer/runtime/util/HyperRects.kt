233ee1ce094d184d9cf45fd7aafe3065dde9c2c9
leadrian <thewishwithin@gmail.com>
2019-02-06 00:31:41 +0100

    Combined Vectors and Rectangles

--- kamp-streamer/src/main/kotlin/ch/leadrian/samp/kamp/streamer/runtime/util/HyperRects.kt
+++ kamp-streamer/src/main/kotlin/ch/leadrian/samp/kamp/streamer/runtime/util/HyperRects.kt
@@ -1,6 +1,9 @@
 package ch.leadrian.samp.kamp.streamer.runtime.util
 
+import ch.leadrian.samp.kamp.core.api.data.Box
+import ch.leadrian.samp.kamp.core.api.data.Rectangle
 import ch.leadrian.samp.kamp.core.api.data.Vector3D
+import com.conversantmedia.util.collection.geometry.Rect2d
 import com.conversantmedia.util.collection.geometry.Rect3d
 
 fun Vector3D.toRect3d(offset: Float): Rect3d {
@@ -12,6 +15,21 @@ fun Vector3D.toRect3d(offset: Float): Rect3d {
     val maxY = coordinates.y + offset
     val maxZ = coordinates.z + offset
 
+    return Rect3d(
+            minX.toDouble(),
+            minY.toDouble(),
+            minZ.toDouble(),
+            maxX.toDouble(),
+            maxY.toDouble(),
+            maxZ.toDouble()
+    )
+}
+
+fun Rectangle.toRect2d(): Rect2d {
+    return Rect2d(minX.toDouble(), minY.toDouble(), maxX.toDouble(), maxY.toDouble())
+}
+
+fun Box.toRect3d(): Rect3d {
     return Rect3d(
             minX.toDouble(),
             minY.toDouble(),
--- kamp-streamer/src/test/kotlin/ch/leadrian/samp/kamp/streamer/runtime/util/HyperRectsTests.kt
+++ kamp-streamer/src/test/kotlin/ch/leadrian/samp/kamp/streamer/runtime/util/HyperRectsTests.kt
@@ -2,12 +2,23 @@ package ch.leadrian.samp.kamp.streamer.runtime.util
 
 import ch.leadrian.samp.kamp.core.api.data.boxOf
 import ch.leadrian.samp.kamp.core.api.data.rectangleOf
+import ch.leadrian.samp.kamp.core.api.data.vector3DOf
 import com.conversantmedia.util.collection.geometry.Rect2d
 import com.conversantmedia.util.collection.geometry.Rect3d
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-internal class RectanglesTest {
+internal class HyperRectsTests {
+    
+    @Test
+    fun shouldConvertVector3DToRect3d() {
+        val vector = vector3DOf(1f, 2f, 3f)
+
+        val rect3d = vector.toRect3d(10f)
+
+        assertThat(rect3d)
+                .isEqualTo(Rect3d(-9.0, -8.0, -7.0, 11.0, 12.0, 13.0))
+    }
 
     @Test
     fun shouldConvertRectangleToRect2d() {
