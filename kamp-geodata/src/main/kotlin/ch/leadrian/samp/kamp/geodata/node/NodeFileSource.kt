2911e381a2d28520011c06278c8936ae8a296585
leadrian <thewishwithin@gmail.com>
2019-02-08 20:26:44 +0100

    Added @see to NodeFileSource

--- kamp-geodata/src/main/kotlin/ch/leadrian/samp/kamp/geodata/node/NodeFileSource.kt
+++ kamp-geodata/src/main/kotlin/ch/leadrian/samp/kamp/geodata/node/NodeFileSource.kt
@@ -2,6 +2,10 @@ package ch.leadrian.samp.kamp.geodata.node
 
 import java.io.InputStream
 
+/**
+ * @see [DataDirectoryNodeFileSource]
+ * @see [ResourcesNodeFileSource]
+ */
 interface NodeFileSource {
 
     fun getNodeFileContent(fileName: String): InputStream
