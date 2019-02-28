3b870ef1c123d542f315204a7c827def1a9d759f
leadrian <thewishwithin@gmail.com>
2019-02-28 23:54:03 +0100

    Added logger() extension method

--- kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/util/Loggers.kt
+++ kamp-core/src/main/kotlin/ch/leadrian/samp/kamp/core/api/util/Loggers.kt
@@ -4,3 +4,5 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 inline fun <reified T : Any> loggerFor(): Logger = LoggerFactory.getLogger(T::class.java)
+
+fun Any.logger(): Logger = LoggerFactory.getLogger(this::class.java)
