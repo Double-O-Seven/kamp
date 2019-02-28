6bb7ed3966bcb1f244506dd9a03de46e5b9b2491
leadrian <thewishwithin@gmail.com>
2019-02-28 23:55:49 +0100

    Added OnRconLoginAttempt test callback

--- examples/amx-interop-test/src/main/kotlin/ch/leadrian/samp/kamp/examples/amxinteroptest/AmxCallbacks.kt
+++ examples/amx-interop-test/src/main/kotlin/ch/leadrian/samp/kamp/examples/amxinteroptest/AmxCallbacks.kt
@@ -7,6 +7,7 @@ import ch.leadrian.samp.kamp.core.api.entity.id.VehicleId
 import ch.leadrian.samp.kamp.core.api.service.PlayerService
 import ch.leadrian.samp.kamp.core.api.service.VehicleService
 import ch.leadrian.samp.kamp.core.api.text.MessageSender
+import ch.leadrian.samp.kamp.core.api.util.logger
 import javax.annotation.PostConstruct
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -21,6 +22,8 @@ constructor(
         private val vehicleService: VehicleService
 ) {
 
+    private val log = logger()
+
     @PostConstruct
     fun initialize() {
         amxCallbackFactory.create2<Int, String>("OnPlayerText") { playerId, text ->
@@ -28,10 +31,18 @@ constructor(
             messageSender.sendMessageToPlayer(player, Colors.WHITE, "You said: {0}", text)
             1
         }
-        amxCallbackFactory.create3<Int, Int, Int>("OnPlayerEnterVehicle") { playerId, vehicleId, isPassenger ->
+        amxCallbackFactory.create3<Int, Int, Int>("OnPlayerEnterVehicle") { playerId, vehicleId, _ ->
             val player = playerService.getPlayer(PlayerId.valueOf(playerId))
             val vehicle = vehicleService.getVehicle(VehicleId.valueOf(vehicleId))
             messageSender.sendMessageToPlayer(player, Colors.WHITE, "Entering {0}...", vehicle.model)
+            1
+        }
+        amxCallbackFactory.create3<String, String, Int>("OnRconLoginAttempt") { ip, password, success ->
+            if (success == 0) {
+                log.warn("Failed RCON login attempt from {}: {}", ip, password)
+            } else {
+                log.info("Successful RCON login from {}", ip)
+            }
             1
         }
     }
